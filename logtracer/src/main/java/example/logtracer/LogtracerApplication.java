25d5957865755d7d67bce707654cd2ef8568b868
Chan <33cks1423@naver.com>
2022-12-29 16:56:44 +0900

    test

---
+++ logtracer/src/main/java/example/logtracer/LogtracerApplication.java
@@ -0,0 +1,13 @@
+package example.logtracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogtracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogtracerApplication.class, args);
+	}
+
+}
