e148a792ef26353bf38885504f8267322630c9e3
Chan <33cks1423@naver.com>
2023-04-19 17:40:31 +0900

    [#2] 새로운 할인 정책 적용과 문제점

--- SpringBasic/MakeExample/demo/src/main/java/com/example/demo/order/OrderServiceImpl.java
+++ SpringBasic/MakeExample/demo/src/main/java/com/example/demo/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.order;
 
 import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.discount.FixDiscountPolicy;
+import com.example.demo.discount.RateDiscountPolicy;
 import com.example.demo.member.Member;
 import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemoryMemberRepository;
@@ -9,8 +10,8 @@ import com.example.demo.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
