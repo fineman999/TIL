6c89e47963b329e939c3ad302562d30945484c82
Chan <33cks1423@naver.com>
2023-04-19 22:20:44 +0900

    [#2] 스프링으로 전환하기

--- SpringBasic/MakeExample/demo/src/main/java/com/example/demo/AppConfig.java
+++ SpringBasic/MakeExample/demo/src/main/java/com/example/demo/AppConfig.java
@@ -2,27 +2,32 @@ package com.example.demo;
 
 import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.discount.FixDiscountPolicy;
+import com.example.demo.discount.RateDiscountPolicy;
 import com.example.demo.member.MemberService;
 import com.example.demo.member.MemberServiceImpl;
 import com.example.demo.member.MemoryMemberRepository;
 import com.example.demo.order.Order;
 import com.example.demo.order.OrderService;
 import com.example.demo.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
