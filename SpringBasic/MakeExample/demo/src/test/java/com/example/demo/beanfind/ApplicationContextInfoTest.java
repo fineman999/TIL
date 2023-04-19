ce5a99ac0398771a7826f0e01bd5c9fd789ee93d
Chan <33cks1423@naver.com>
2023-04-19 22:49:03 +0900

    [#3] 컨테이너에 등록된 모든 빈 조회

---
+++ SpringBasic/MakeExample/demo/src/test/java/com/example/demo/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.beanfind;
+
+import com.example.demo.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName);
+            System.out.println("bean = " + bean);
+        }
+    }
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            // 내가 등록한 빈들: ROLE_APPLICATION
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + ", bean = " + bean);
+            }
+        }
+    }
+}
