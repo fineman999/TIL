4f9ecc108f111b2ba6ac06950a895ac259229b79
Chan <33cks1423@naver.com>
2023-04-19 16:08:20 +0900

    [#1] 회원 도메인 실행과 테스트

---
+++ SpringBasic/MakeExample/demo/src/main/java/com/example/demo/MemberApp.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args){
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
