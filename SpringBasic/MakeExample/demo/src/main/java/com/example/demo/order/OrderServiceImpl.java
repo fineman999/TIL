3318f7eeba7698a19460f4389aa3b93df4b36f1f
Chan <33cks1423@naver.com>
2023-04-21 21:58:59 +0900

    [#5] 옵션 처리

--- SpringBasic/MakeExample/demo/src/main/java/com/example/demo/order/OrderServiceImpl.java
+++ SpringBasic/MakeExample/demo/src/main/java/com/example/demo/order/OrderServiceImpl.java
@@ -9,9 +9,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
-
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
