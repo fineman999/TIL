2326f3466bcf7df63c2418e90157fb589be4a870
Chan <33cks1423@naver.com>
2023-04-21 18:44:59 +0900

    [#5] 필터

---
+++ SpringBasic/MakeExample/demo/src/test/java/com/example/demo/scan/filter/BeanA.java
@@ -0,0 +1,4 @@
+package com.example.demo.scan.filter;
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ SpringBasic/MakeExample/demo/src/test/java/com/example/demo/scan/filter/BeanB.java
@@ -0,0 +1,4 @@
+package com.example.demo.scan.filter;
+@MyExcludeComponent
+public class BeanB {
+}
