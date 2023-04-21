6353e257a2c8f86a833b96e40736c518f9f72bfc
Chan <33cks1423@naver.com>
2023-04-21 15:55:48 +0900

    [#4] 싱글톤 컨테이너

--- SpringBasic/MakeExample/demo/src/test/java/com/example/demo/singleton/SingletonTest.java
+++ SpringBasic/MakeExample/demo/src/test/java/com/example/demo/singleton/SingletonTest.java
@@ -4,6 +4,7 @@ import com.example.demo.AppConfig;
 import com.example.demo.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,5 +36,23 @@ public class SingletonTest {
 
         assertThat(singletonService1).isSameAs(singletonService2);
         // same: 인스턴스가 같은지 확인
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        // 2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // 달라야 됨 -> 객체가 4개가 생김
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
