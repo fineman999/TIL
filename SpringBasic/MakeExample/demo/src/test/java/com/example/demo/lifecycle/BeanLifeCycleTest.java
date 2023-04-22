fa2ebd0e430a4e2acb388d3d6d6009e0f071a289
Chan <33cks1423@naver.com>
2023-04-22 22:04:22 +0900

    [#7] 빈 생명주기 콜백 시작

---
+++ SpringBasic/MakeExample/demo/src/test/java/com/example/demo/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void liefCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://naver.com");
+            return networkClient;
+        }
+    }
+}
