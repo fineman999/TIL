e396a2f5bca651709c1689d5bda6a1c69dbc0461
Chan <33cks1423@naver.com>
2023-04-22 22:17:31 +0900

    [#7] 인터페이스 InitializingBean, DisposableBean

--- SpringBasic/MakeExample/demo/src/test/java/com/example/demo/lifecycle/NetworkClient.java
+++ SpringBasic/MakeExample/demo/src/test/java/com/example/demo/lifecycle/NetworkClient.java
@@ -1,12 +1,13 @@
 package com.example.demo.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -19,11 +20,24 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call: " + url + "message = " + message);
+        System.out.println("call: " + url + " message = " + message);
     }
 
     // 서비스 호출시 나가기
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
