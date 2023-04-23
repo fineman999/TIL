0f5bf7394b4da9e65ba4adee9eca59bdbedc62b6
Chan <33cks1423@naver.com>
2023-04-23 16:47:30 +0900

    [#8] 스코프와 Provider

--- SpringBasic/MakeExample/demo/src/main/java/com/example/demo/web/LogDemoController.java
+++ SpringBasic/MakeExample/demo/src/main/java/com/example/demo/web/LogDemoController.java
@@ -3,6 +3,7 @@ package com.example.demo.web;
 import com.example.demo.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
-    @RequestMapping("log-memo")
+    @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURI();
+
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- SpringBasic/MakeExample/demo/src/main/java/com/example/demo/web/LogDemoService.java
+++ SpringBasic/MakeExample/demo/src/main/java/com/example/demo/web/LogDemoService.java
@@ -2,13 +2,15 @@ package com.example.demo.web;
 
 import com.example.demo.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String testId) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + testId);
     }
 }
