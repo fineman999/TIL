0b520fe83263e7ea83508e09411ff05996543fe3
Chan <33cks1423@naver.com>
2023-04-23 17:10:46 +0900

    [#8] 스코프와 프록시

--- SpringBasic/MakeExample/demo/src/main/java/com/example/demo/web/LogDemoController.java
+++ SpringBasic/MakeExample/demo/src/main/java/com/example/demo/web/LogDemoController.java
@@ -3,7 +3,6 @@ package com.example.demo.web;
 import com.example.demo.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,14 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURI();
-
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- SpringBasic/MakeExample/demo/src/main/java/com/example/demo/web/LogDemoService.java
+++ SpringBasic/MakeExample/demo/src/main/java/com/example/demo/web/LogDemoService.java
@@ -2,15 +2,14 @@ package com.example.demo.web;
 
 import com.example.demo.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     public void logic(String testId) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+
         myLogger.log("service id = " + testId);
     }
 }
