6e5ff5c5e15961bb64ba741a7e474e22ec3be3c2
Chan <33cks1423@naver.com>
2023-04-23 15:30:58 +0900

    [#8] 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제

--- SpringBasic/MakeExample/demo/src/test/java/com/example/demo/scope/SingletonWithPrototypeTest1.java
+++ SpringBasic/MakeExample/demo/src/test/java/com/example/demo/scope/SingletonWithPrototypeTest1.java
@@ -2,10 +2,12 @@ package com.example.demo.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,20 +37,19 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanObjectProvider;
+
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
