cb542f5af8c1908fb6fabbc8d782dcd1b97de8a6
Chan <33cks1423@naver.com>
2023-04-24 21:03:59 +0900

    [#12] 테스트 코드를 이용한 테스트

---
+++ SpringToby/understand_and_principle/helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,31 @@
+package tobyspring.helloboot;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HelloApiTest {
+    @Test
+    @DisplayName("APi TEST")
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code 200
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+
+    }
+}
