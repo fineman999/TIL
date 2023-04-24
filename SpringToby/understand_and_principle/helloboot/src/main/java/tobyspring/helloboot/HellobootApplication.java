3976550423537ee389c9d6976b2d29c8b4937104
Chan <33cks1423@naver.com>
2023-04-24 14:19:09 +0900

    [#10] 서블릿 컨테이너 띄우기

--- SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,16 @@
 package tobyspring.helloboot;
 
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Containerless Standalone Application");
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
