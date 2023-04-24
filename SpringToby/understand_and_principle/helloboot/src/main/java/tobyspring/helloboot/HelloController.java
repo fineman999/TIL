c99abb2049a90eed9019a1c7062f0c15a9174874
Chan <33cks1423@naver.com>
2023-04-24 17:20:45 +0900

    [#11] 애노테이션 매핑 정보 사용

--- SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,13 @@
 package tobyspring.helloboot;
 
-import java.util.Objects;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Objects;
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,7 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
