6d04eac26d931967c37a2d346380f2ebe67965b8
Chan <33cks1423@naver.com>
2023-04-24 16:51:46 +0900

    [#11] 의존 오브젝트 DI 적용

--- SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,11 +3,14 @@ package tobyspring.helloboot;
 import java.util.Objects;
 
 public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
 
     public String hello(String name) {
-        SimpleHelloService simpleHelloService = new SimpleHelloService();
-
-        return simpleHelloService.sayHello(Objects.requireNonNull(name));
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
