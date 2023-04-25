56493c78b5cfd0c859e10d2f033880ad9d87e2bd
Chan <33cks1423@naver.com>
2023-04-25 16:12:17 +0900

    [#13] 동적인 자동 구성 정보 등록

--- SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
+++ SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -1,9 +1,6 @@
 package tobyspring.config;
 
 import org.springframework.context.annotation.Import;
-import tobyspring.config.autoconfig.DispatcherServletConfig;
-import tobyspring.config.autoconfig.TomcatWebServerConfig;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,6 +8,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 }
