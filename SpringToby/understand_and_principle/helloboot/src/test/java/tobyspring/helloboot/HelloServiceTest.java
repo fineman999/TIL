4bc1948c801ed38944825e903035981dc28b7c33
Chan <33cks1423@naver.com>
2023-04-25 15:26:21 +0900

    [#13] 합성 애노테이션의 적용

--- SpringToby/understand_and_principle/helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ SpringToby/understand_and_principle/helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -3,26 +3,8 @@ package tobyspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@UnitTest
-@interface FastUnitTest{
-
-}
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
-@Test
-@interface UnitTest {
-}
 public class HelloServiceTest {
-    @FastUnitTest
+    @Test
     void simpleHelloService() {
         SimpleHelloService simpleHelloService = new SimpleHelloService();
 
