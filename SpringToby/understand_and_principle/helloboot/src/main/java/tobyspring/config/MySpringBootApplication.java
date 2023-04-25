e983a8ae3131e5fe3cc650dd050e91aa7f67417a
Chan <33cks1423@naver.com>
2023-04-25 16:01:48 +0900

    [#13] 인프라 빈 구성 정보의 분리

--- SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/config/MySpringBootApplication.java
+++ SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/config/MySpringBootApplication.java
@@ -1,8 +1,7 @@
-package tobyspring.helloboot;
+package tobyspring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,6 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Configuration
 @ComponentScan
-public @interface MySpringBootAnnotation {
+@EnableMyAutoConfiguration
+public @interface MySpringBootApplication {
 
 }
--- SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ SpringToby/understand_and_principle/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,8 +1,9 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
+import tobyspring.config.MySpringBootApplication;
 
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class HellobootApplication {
 
     public static void main(String[] args) {
