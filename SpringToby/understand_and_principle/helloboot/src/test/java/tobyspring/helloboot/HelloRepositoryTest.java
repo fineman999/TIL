25b2e76cd7cd8101d8e7e685f51f265a2ba37643
Chan <33cks1423@naver.com>
2023-04-28 11:55:46 +0900

    [#15] Hello 리포지토리

---
+++ SpringToby/understand_and_principle/helloboot/src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -0,0 +1,35 @@
+package tobyspring.helloboot;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.*;
+
+@HellobootTest
+public class HelloRepositoryTest {
+    @Autowired JdbcTemplate jdbcTemplate;
+    @Autowired HelloRepository helloRepository;
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+    @Test
+    void findHelloFailed() {
+        assertThat(helloRepository.findHello("Toby")).isNull();
+    }
+
+    @Test
+    void increaseCount() {
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(0);
+
+        helloRepository.increaseCount("Toby");
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(1);
+
+        helloRepository.increaseCount("Toby");
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(2);
+
+    }
+}
--- SpringToby/understand_and_principle/helloboot/src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
+++ SpringToby/understand_and_principle/helloboot/src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.Assertions.*;
 
 @HellobootTest
-@Rollback(value = false)
 public class JdbcTemplateTest {
     private final JdbcTemplate jdbcTemplate;
 
