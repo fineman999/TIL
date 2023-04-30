6a0a378177b7896f0da432bff6cc66f6456de786
Chan <33cks1423@naver.com>
2023-04-30 15:22:29 +0900

    [#18] HTTP 응답 데이터 - API JSON

---
+++ SpringBasic/SpringMVC/servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ResponseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setAge(20);
+        helloData.setUsername("kim");
+
+        String result = objectMapper.writeValueAsString(helloData);
+        resp.getWriter().write(result);
+    }
+}
