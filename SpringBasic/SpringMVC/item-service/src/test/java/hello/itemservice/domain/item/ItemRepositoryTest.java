9801743e8c4682531cc6a94dc231e9ec7156e32f
Chan <33cks1423@naver.com>
2023-05-04 19:37:37 +0900

    [#23] 상품 도메인 개발

---
+++ SpringBasic/SpringMVC/item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,63 @@
+package hello.itemservice.domain.item;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        Item item = new Item("itemA", 10000, 10);
+
+        Item savedItem = itemRepository.save(item);
+
+        Item findItem = itemRepository.findById(item.getId());
+
+        assertThat(findItem).isEqualTo(savedItem);
+
+    }
+
+    @Test
+    void findAll() {
+        Item item1 = new Item("itemA", 10000, 10);
+        Item item2 = new Item("itemB", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        List<Item> result = itemRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+    }
+
+    @Test
+    void updateItem() {
+        Item item = new Item("itemA", 10000, 10);
+
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        Item updateParam = new Item("item2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+
+        Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+
+    }
+
+}
