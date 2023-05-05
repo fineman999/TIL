d6878b39ecd63b2e364fb268c3b6fcdd7e4f7bfc
Chan <33cks1423@naver.com>
2023-05-05 21:44:04 +0900

    [#24] 템플릿 레이아웃1

--- SpringBasic/SpringMVC/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
+++ SpringBasic/SpringMVC/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
@@ -12,4 +12,9 @@ public class TemplateController {
     public String template() {
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
 }
