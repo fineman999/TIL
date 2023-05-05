de0e85dd83293ddd29c537ff90a4ecee725d36fd
Chan <33cks1423@naver.com>
2023-05-05 17:19:56 +0900

    [#24] 기본 객체들

--- SpringBasic/SpringMVC/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ SpringBasic/SpringMVC/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -1,6 +1,10 @@
 package hello.thymeleafbasic.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,7 +48,23 @@ public class BasicController {
         model.addAttribute("userMap", map);
 
         return "basic/variable";
+    }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                   HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
 
     @Data
