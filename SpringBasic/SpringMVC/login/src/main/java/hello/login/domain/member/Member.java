4706dad37bce4b06de5dd4f2da6db58fa27bef90
Chan <33cks1423@naver.com>
2023-05-08 23:00:49 +0900

    [#29] 회원 가입

---
+++ SpringBasic/SpringMVC/login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,20 @@
+package hello.login.domain.member;
+
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long id;
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String password;
+}
