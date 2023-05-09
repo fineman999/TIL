93016601c4e7533373104f9aeeab5660564de012
Chan <33cks1423@naver.com>
2023-05-09 20:27:16 +0900

    [#29] 로그인 처리하기 - 서블릿 HTTP 세션2

--- SpringBasic/SpringMVC/login/src/main/java/hello/login/web/HomeController.java
+++ SpringBasic/SpringMVC/login/src/main/java/hello/login/web/HomeController.java
@@ -1,7 +1,6 @@
 package hello.login.web;
 
 import hello.login.domain.member.Member;
-import hello.login.web.session.SessionManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -9,13 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(HttpServletRequest request, Model model) {
         HttpSession session = request.getSession(false);
         if (session == null) {
@@ -32,5 +32,15 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "login/loginHome";
     }
+    @GetMapping("/")
+    public String homeLoginSpring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+                                  Model model) {
 
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "login/loginHome";
+    }
 }
