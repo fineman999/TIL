ca8b2acd6624d39b64617a4c6d52c233443b8f06
Chan <33cks1423@naver.com>
2023-05-09 21:46:32 +0900

    [#30] 서블릿 필터 - 요청 로그

---
+++ SpringBasic/SpringMVC/login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,21 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
---
+++ SpringBasic/SpringMVC/login/src/main/java/hello/login/web/filter/LogFilter.java
@@ -0,0 +1,41 @@
+package hello.login.web.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+        // 다운 캐스팅
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        String requestURI = httpRequest.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+        try {
+            log.info("REQUEST [{}][{}]",uuid, requestURI);
+            // 다음 체인 없으면 서블릿 호출
+            chain.doFilter(request, response);
+        }catch (Exception e) {
+            throw e;
+        }finally {
+            // 서블릿 까지 다 호출하고 끝나면
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
