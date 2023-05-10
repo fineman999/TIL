fa88fadcaf34ef7cb56a07ef716c167fccef6438
Chan <33cks1423@naver.com>
2023-05-10 21:29:31 +0900

    [#31] 스프링 부트 - 오류 페이지1

--- SpringBasic/SpringMVC/exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ SpringBasic/SpringMVC/exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -20,6 +20,10 @@ public class ServletExController {
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!");
     }
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류!");
+    }
 
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
