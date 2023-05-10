d40e73ee247b673443ddd25771ffb64f30ded38a
Chan <33cks1423@naver.com>
2023-05-10 19:41:49 +0900

    [#31] 서블릿 예외 처리 - 오류 화면 제공

---
+++ SpringBasic/SpringMVC/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -0,0 +1,25 @@
+package hello.exception.servlet;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ErrorPageController {
+
+    @GetMapping("/error-page/404")
+    public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
+        log.info("errorPage 404");
+        return "error-page/404";
+    }
+
+    @GetMapping("/error-page/500")
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
+        log.info("errorPage 500");
+        return "error-page/500";
+    }
+
+}
