41714ecf432fcc2d58ff0f085f5eb3cf59a5b18c
Chan <33cks1423@naver.com>
2023-05-10 20:17:51 +0900

    [#31] 서블릿 예외 처리 - 오류 페이지 작동 원리

--- SpringBasic/SpringMVC/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ SpringBasic/SpringMVC/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,5 +1,6 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -10,16 +11,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorPageController {
 
+
+
     @GetMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 404");
+        printErrorInfo(request);
         return "error-page/404";
     }
 
     @GetMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 500");
+        printErrorInfo(request);
         return "error-page/500";
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION={}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE={}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE={}", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI={}", request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME={}", request.getAttribute(RequestDispatcher.ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE={}", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
+        log.info("dispatchType={}", request.getDispatcherType());
+
     }
 
 }
