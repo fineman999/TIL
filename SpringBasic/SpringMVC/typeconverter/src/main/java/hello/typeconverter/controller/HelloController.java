0db2c5ff489e426bbf3665e50d5a71311c98a37b
Chan <33cks1423@naver.com>
2023-05-11 21:14:07 +0900

    [#33] 스프링 타입 컨버터 소개

---
+++ SpringBasic/SpringMVC/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,26 @@
+package hello.typeconverter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        int intValue = Integer.parseInt(data);
+        log.info("intValue={}", intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        log.info("data={}", data);
+        return "ok";
+    }
+}
