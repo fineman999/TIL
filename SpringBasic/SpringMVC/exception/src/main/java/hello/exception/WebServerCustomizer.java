c78aecd3f4646aed0855ee7c06857a836de4e48a
Chan <33cks1423@naver.com>
2023-05-11 10:43:25 +0900

    [#32] API 예외 처리 - 시작

--- SpringBasic/SpringMVC/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ SpringBasic/SpringMVC/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -4,8 +4,9 @@ import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
--- SpringBasic/SpringMVC/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ SpringBasic/SpringMVC/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,11 +1,18 @@
 package hello.exception.servlet;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static jakarta.servlet.RequestDispatcher.*;
 
 @Slf4j
 @Controller
@@ -20,20 +27,35 @@ public class ErrorPageController {
         return "error-page/404";
     }
 
-    @GetMapping("/error-page/500")
+    @GetMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(
+            HttpServletRequest request, HttpServletResponse response) {
+        log.info("API errorPage 500");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+        Integer statusCode = (Integer) request.getAttribute(ERROR_STATUS_CODE);
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+
+    }
+
+    @GetMapping(value = "/error-page/500", produces = MediaType.TEXT_HTML_VALUE)
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 500");
         printErrorInfo(request);
         return "error-page/500";
     }
 
+
     private void printErrorInfo(HttpServletRequest request) {
-        log.info("ERROR_EXCEPTION={}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
-        log.info("ERROR_EXCEPTION_TYPE={}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE));
-        log.info("ERROR_MESSAGE={}", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
-        log.info("ERROR_REQUEST_URI={}", request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
-        log.info("ERROR_SERVLET_NAME={}", request.getAttribute(RequestDispatcher.ERROR_SERVLET_NAME));
-        log.info("ERROR_STATUS_CODE={}", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
+        log.info("ERROR_EXCEPTION={}", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE={}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE={}", request.getAttribute(ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI={}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME={}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE={}", request.getAttribute(ERROR_STATUS_CODE));
         log.info("dispatchType={}", request.getDispatcherType());
 
     }
