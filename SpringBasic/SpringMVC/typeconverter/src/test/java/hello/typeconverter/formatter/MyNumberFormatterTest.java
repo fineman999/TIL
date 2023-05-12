d6fe1e818baf34ca2c3b1462b8f60616b28993c0
Chan <33cks1423@naver.com>
2023-05-12 19:57:24 +0900

    [#33] 포맷터 - Formatter

---
+++ SpringBasic/SpringMVC/typeconverter/src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,24 @@
+package hello.typeconverter.formatter;
+
+import org.junit.jupiter.api.Test;
+import java.text.ParseException;
+import java.util.Locale;
+import static org.assertj.core.api.Assertions.*;
+
+
+class MyNumberFormatterTest {
+    MyNumberFormatter formatter = new MyNumberFormatter();
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+        assertThat(result).isEqualTo(1000L);
+
+    }
+
+    @Test
+    void print() {
+
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
