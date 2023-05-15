157ffed519edc1965db18422d3271f90e50c42ca
Chan <33cks1423@naver.com>
2023-05-15 11:50:50 +0900

    [#35] 데이터베이스 연결

--- SpringBasic/SpringDB/jdbc/src/main/java/hello/jdbc/JdbcApplication.java
+++ SpringBasic/SpringDB/jdbc/src/main/java/hello/jdbc/JdbcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JdbcApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(JdbcApplication.class, args);
     }
