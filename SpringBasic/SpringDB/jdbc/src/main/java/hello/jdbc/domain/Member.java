cb72bd896ea9b752b44231d4935ff78c6a9892a9
Chan <33cks1423@naver.com>
2023-05-15 12:22:32 +0900

    [#35] JDBC 개발 - 등록

---
+++ SpringBasic/SpringDB/jdbc/src/main/java/hello/jdbc/domain/Member.java
@@ -0,0 +1,12 @@
+package hello.jdbc.domain;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    private String memberId;
+    private int money;
+}
+
