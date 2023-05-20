5c33fe0bd64a40f60117eedd1ec73cae737adfbd
Chan <33cks1423@naver.com>
2023-05-20 16:30:00 +0900

    [#42] JdbcTemplate - 이름 지정 파라미터 3

--- SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,13 +1,13 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV1Config;
+import hello.itemservice.config.JdbcTemplateV2Config;
 import hello.itemservice.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-@Import(JdbcTemplateV1Config.class)
+@Import(JdbcTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
