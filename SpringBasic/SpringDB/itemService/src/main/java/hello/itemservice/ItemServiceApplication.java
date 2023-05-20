516c3e013acd237e7f257a8d03b689b13dbb6a23
Chan <33cks1423@naver.com>
2023-05-20 15:20:34 +0900

    [#42] JdbcTemplate 적용3 - 구성과 실행

--- SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,13 +1,13 @@
 package hello.itemservice;
 
-import hello.itemservice.config.MemoryConfig;
+import hello.itemservice.config.JdbcTemplateV1Config;
 import hello.itemservice.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-@Import(MemoryConfig.class)
+@Import(JdbcTemplateV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
