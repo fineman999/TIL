08484ba67e3a17bab34f77cbddc3d8a7691aedac
Chan <33cks1423@naver.com>
2023-05-21 17:58:49 +0900

    [#44] MyBatis 적용2 - 설정과 실행

--- SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 @Slf4j
-@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
