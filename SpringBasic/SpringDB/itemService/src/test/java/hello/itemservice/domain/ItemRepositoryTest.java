57ed72a177e64173e402ab9d29a73ae0e485370f
Chan <33cks1423@naver.com>
2023-05-21 15:31:18 +0900

    [#43] 테스트 - 임베디드 모드 DB

--- SpringBasic/SpringDB/itemService/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ SpringBasic/SpringDB/itemService/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -31,8 +30,7 @@ class ItemRepositoryTest {
         }
     }
 
-    @Commit
-    @Transactional
+
     @Test
     void save() {
         //given
