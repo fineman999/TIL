4caf4bfff0fa68f8a098cb7945bd9a176da19d16
Chan <33cks1423@naver.com>
2023-05-21 15:42:10 +0900

    [#43] 테스트 - 스프링 부트와 임베디드 모드

--- SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Slf4j
 @Import(JdbcTemplateV3Config.class)
@@ -28,15 +25,4 @@ public class ItemServiceApplication {
         return new TestDataInit(itemRepository);
     }
 
-    @Bean
-    @Profile("test")
-    public DataSource dataSource() {
-        log.info("메모리 데이터베이스 초기화");
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-        return dataSource;
-    }
 }
