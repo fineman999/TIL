60fe06e5071754e064f56025b6d40e22e0b2adf0
Chan <33cks1423@naver.com>
2023-05-22 23:07:29 +0900

    [#45] JPA 적용3 - 예외 변환

--- SpringBasic/SpringDB/itemService/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ SpringBasic/SpringDB/itemService/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -4,6 +4,7 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 @Transactional
 @SpringBootTest
 class ItemRepositoryTest {
@@ -69,6 +71,7 @@ class ItemRepositoryTest {
         Item item2 = new Item("itemA-2", 20000, 20);
         Item item3 = new Item("itemB-1", 30000, 30);
 
+        log.info("repository={}", itemRepository.getClass());
         itemRepository.save(item1);
         itemRepository.save(item2);
         itemRepository.save(item3);
