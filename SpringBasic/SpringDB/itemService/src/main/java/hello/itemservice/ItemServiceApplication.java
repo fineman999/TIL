9bf6d43fe0c980413abc62419cc041fea62010dd
Chan <33cks1423@naver.com>
2023-05-23 19:28:05 +0900

    [#49] 실용적인 구조

--- SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ SpringBasic/SpringDB/itemService/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
-import hello.itemservice.config.QuerydslConfig;
+
+import hello.itemservice.config.V2Config;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 @Slf4j
-@Import(QuerydslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
