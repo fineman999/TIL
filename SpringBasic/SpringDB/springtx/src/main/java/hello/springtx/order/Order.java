774918988205973a5b1cb7b20c88d9c3ba541d09
Chan <33cks1423@naver.com>
2023-05-24 21:00:31 +0900

    [#50] 예외와 트랜잭션 커밋, 롤백 - 활용

---
+++ SpringBasic/SpringDB/springtx/src/main/java/hello/springtx/order/Order.java
@@ -0,0 +1,25 @@
+package hello.springtx.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username; //정상, 예외, 잔고부족
+    private String payStatus; // 대기, 완료
+
+
+}
---
+++ SpringBasic/SpringDB/springtx/src/test/java/hello/springtx/order/OrderServiceTest.java
@@ -0,0 +1,61 @@
+package hello.springtx.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class OrderServiceTest {
+
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository orderRepository;
+
+
+    @Test
+    void order() throws NotEnoughMoneyException {
+        // given
+        Order order = new Order();
+        order.setUsername("정상");
+        // when
+        orderService.order(order);
+
+        Order findOrder = orderRepository.findById(order.getId()).get();
+        assertThat(findOrder.getPayStatus()).isEqualTo("완료");
+    }
+
+    @Test
+    void runtimeException() {
+        // given
+        Order order = new Order();
+        order.setUsername("예외");
+        // when
+        assertThatThrownBy(() -> orderService.order(order))
+                .isInstanceOf(RuntimeException.class);
+
+
+        Optional<Order> orderOptional = orderRepository.findById(order.getId());
+        assertThat(orderOptional.isEmpty()).isTrue();
+    }
+
+    @Test
+    void bizException() {
+        Order order = new Order();
+        order.setUsername("잔고부족");
+
+        try {
+            orderService.order(order);
+        } catch (NotEnoughMoneyException e) {
+            log.info("고객에게 잔고 부족을 알리고 별도의 계좌로 입금하도록 안내");
+        }
+
+        Order findOrder = orderRepository.findById(order.getId()).get();
+        assertThat(findOrder.getPayStatus()).isEqualTo("대기");
+
+    }
+}
