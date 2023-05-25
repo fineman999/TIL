d10c20a8a764a1e3ab510810a8c7b45c0140dd23
Chan <33cks1423@naver.com>
2023-05-25 15:28:10 +0900

    [#51] 스프링 트랜잭션 전파6 - 내부 롤백

--- SpringBasic/SpringDB/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ SpringBasic/SpringDB/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -9,9 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
@@ -113,5 +116,20 @@ public class BasicTxTest {
         txManager.rollback(outer);
     }
 
+    @Test
+    void inner_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
 
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionDefinition());
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner); // rollback-only 표시
+
+        log.info("외부 트랜잭션 커밋");
+        assertThatThrownBy(() -> txManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
+    }
 }
