3ca643a36285b25517ac453d35c28363fb3e323f
Chan <33cks1423@naver.com>
2023-05-25 12:38:26 +0900

    [#51] 스프링 트랜잭션 전파4 - 전파 예제 - 성공

--- SpringBasic/SpringDB/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ SpringBasic/SpringDB/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -79,6 +79,24 @@ public class BasicTxTest {
 
     }
 
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+
+
+    }
+
 
 
 }
