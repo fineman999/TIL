6cb8da614ac8385203d6c8074d2ed2d9db02f0de
Chan <33cks1423@naver.com>
2023-05-26 14:27:28 +0900

    [#53] 로그 추적기 V1 - 적용

---
+++ SpringAOP/advanced/src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,31 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request/{itemId}")
+    public String request(@PathVariable String itemId) {
+
+        TraceStatus status = trace.begin("OrderController.request()");
+        try {
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 한다.
+        }
+
+    }
+}
