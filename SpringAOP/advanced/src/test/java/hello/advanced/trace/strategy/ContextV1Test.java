649a9dee2549cfcfbc998dcd4dc5a89d8489da65
Chan <33cks1423@naver.com>
2023-05-27 17:10:09 +0900

    [#55] 전략 패턴 - 예제2

--- SpringAOP/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ SpringAOP/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -47,5 +48,22 @@ public class ContextV1Test {
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 context2 = new ContextV1(strategyLogic2);
         context2.execute();
+    }
+
+    @Test
+    void strategyV2() {
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직 1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        });
+        context2.execute();
+
+
     }
 }
