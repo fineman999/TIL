d56523b755a3166661fc88f1fa9fc820693fcc06
Chan <33cks1423@naver.com>
2023-05-27 16:55:22 +0900

    [#55] 전략 패턴 - 예제1

--- SpringAOP/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ SpringAOP/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,8 +1,8 @@
 package hello.advanced.trace.strategy;
 
-import hello.advanced.trace.template.code.AbstractTemplate;
-import hello.advanced.trace.template.code.SubClassLogic1;
-import hello.advanced.trace.template.code.SubClassLogic2;
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -36,37 +36,16 @@ public class ContextV1Test {
     }
 
     /**
-     * 템플릿 메서드 패턴 적용
+     * 전략 패턴 적용
      */
     @Test
-    void templateMethodV1() {
-        AbstractTemplate template1 = new SubClassLogic1();
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
 
-        template1.execute();
-
-        AbstractTemplate template2 = new SubClassLogic2();
-        template2.execute();
-    }
-
-
-    @Test
-    void templateMethodV2() {
-        AbstractTemplate template1 = new AbstractTemplate(){
-            @Override
-            protected void call() {
-                log.info("비즈니스 로직1 실행");
-            }
-        };
-        log.info("클래스 이름1={}", template1.getClass());
-        template1.execute();
-        AbstractTemplate template2 = new AbstractTemplate(){
-            @Override
-            protected void call() {
-                log.info("비즈니스 로직2 실행");
-            }
-        };
-        log.info("클래스 이름2={}", template2.getClass());
-        template2.execute();
-
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
     }
 }
