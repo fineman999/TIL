08320f5b999187cfdf995bd42243bbd34bc0a9a9
Chan <33cks1423@naver.com>
2023-05-27 17:29:16 +0900

    [#55] 전략 패턴 - 예제3

---
+++ SpringAOP/advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,31 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(new StrategyLogic1());
+        contextV2.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 (익명 내부 클래스(람다)) 적용
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(() -> log.info("비즈니스 로직1 실행"));
+        contextV2.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+
+
+}
