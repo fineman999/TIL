23a77a45947b0e35456a02b36ed7e490efacf63b
Chan <33cks1423@naver.com>
2023-05-28 21:10:15 +0900

    [#56] 구체 클래스 기반 프록시 - 적용

--- SpringAOP/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringAOP/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 //
 
-@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의
 public class ProxyApplication {
 
---
+++ SpringAOP/proxy/src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderServiceConcreteProxy.java
@@ -0,0 +1,36 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+
+
+public class OrderServiceConcreteProxy extends OrderServiceV2 {
+    private final OrderServiceV2 target;
+    private final LogTrace logTrace;
+
+    public OrderServiceConcreteProxy(OrderServiceV2 target, LogTrace logTrace) {
+        // 위의 구조를 안쓰고 프록시 용도로 쓰기 때문에 null로 표기하자.
+        super(null);
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+
+    @Override
+    public void orderItem(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.orderItem()");
+            // target 호출
+            target.orderItem(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+
+    }
+}
