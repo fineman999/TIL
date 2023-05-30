4c1613c9aaba9f7873b69135f31003ff497c5899
Chan <33cks1423@naver.com>
2023-05-30 19:52:04 +0900

    [#59] 빈 후처리기 - 적용

--- SpringAOP/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringAOP/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,7 @@
 package hello.proxy;
 
 
-
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의
 public class ProxyApplication {
 
