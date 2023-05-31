09a812fd3b1d5f6579107d4e2fca3fd72bcae49a
Chan <33cks1423@naver.com>
2023-05-31 11:00:23 +0900

    [#60] @Aspect 프록시 - 적용

--- SpringAOP/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringAOP/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,7 @@
 package hello.proxy;
 
 
-import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
-import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의
 public class ProxyApplication {
 
