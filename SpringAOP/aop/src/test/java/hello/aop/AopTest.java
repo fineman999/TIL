6e8c38b73b9f7df65ccf3861382915acb86d3b60
Chan <33cks1423@naver.com>
2023-05-31 21:14:29 +0900

    [#62] 스프링 AOP 구현3 - 어드바이스 추가

--- SpringAOP/aop/src/test/java/hello/aop/AopTest.java
+++ SpringAOP/aop/src/test/java/hello/aop/AopTest.java
@@ -3,7 +3,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Import;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
-@Import(AspectV2.class)
+@Import(AspectV3.class)
 @SpringBootTest
 public class AopTest {
 
