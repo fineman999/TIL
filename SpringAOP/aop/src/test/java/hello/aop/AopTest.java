661a23199a1410539787b94c4a501e9be9024589
Chan <33cks1423@naver.com>
2023-05-31 20:17:16 +0900

    [#62] 예제 프로젝트 만들기

---
+++ SpringAOP/aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,42 @@
+package hello.aop;
+
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        assertThatThrownBy(()->orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+
+}
