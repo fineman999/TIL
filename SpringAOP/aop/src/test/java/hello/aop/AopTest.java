29b470b18220fbd75dae2d5a944b84c50bc5d9f5
Chan <33cks1423@naver.com>
2023-05-31 20:44:33 +0900

    [#62] 스프링 AOP 구현2 - 포인트컷 분리

--- SpringAOP/aop/src/test/java/hello/aop/AopTest.java
+++ SpringAOP/aop/src/test/java/hello/aop/AopTest.java
@@ -3,7 +3,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Import;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
-@Import(AspectV1.class)
+@Import(AspectV2.class)
 @SpringBootTest
 public class AopTest {
 
