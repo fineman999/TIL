16efc6767ca15d1c89bb7251eb5653025c8a15a9
Chan <33cks1423@naver.com>
2023-06-04 19:28:30 +0900

    [#68] 편리한 부트 클래스 만들기

--- SpringBoot/embed/src/main/java/hello/spring/HelloConfig.java
+++ SpringBoot/embed/src/main/java/hello/spring/HelloConfig.java
@@ -1,9 +1,8 @@
 package hello.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class HelloConfig {
 
     @Bean
