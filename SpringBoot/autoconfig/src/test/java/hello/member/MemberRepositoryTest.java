04ffac8d59f9808c4e90a5bc177b3454893202cd
Chan <33cks1423@naver.com>
2023-06-05 13:45:44 +0900

    [#70] 예제 만들기

---
+++ SpringBoot/autoconfig/src/test/java/hello/member/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package hello.member;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    @Test
+    void memberTest() {
+        Member member = new Member("idA", "memberA");
+        memberRepository.initTable();
+        memberRepository.save(member);
+        Member findMember = memberRepository.find(member.getMemberId());
+        assertThat(findMember.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
