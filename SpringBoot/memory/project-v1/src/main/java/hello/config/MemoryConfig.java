bc19485f5d7b782d5bc5f26a1d691a7cdd720069
Chan <33cks1423@naver.com>
2023-06-05 16:39:06 +0900

    [#70] 자동 구성 라이브러리 사용하기

--- SpringBoot/memory/project-v1/src/main/java/hello/config/MemoryConfig.java
+++ SpringBoot/memory/project-v1/src/main/java/hello/config/MemoryConfig.java
@@ -5,7 +5,7 @@ import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class MemoryConfig {
 
     @Bean
