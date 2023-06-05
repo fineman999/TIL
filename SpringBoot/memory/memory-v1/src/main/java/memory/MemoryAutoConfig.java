126523e2a586eb0caa7e252b8f33e06f34451a00
Chan <33cks1423@naver.com>
2023-06-05 16:25:07 +0900

    [#70] 자동 구성 라이브러리 만들기

---
+++ SpringBoot/memory/memory-v1/src/main/java/memory/MemoryAutoConfig.java
@@ -0,0 +1,20 @@
+package memory;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@ConditionalOnProperty(name = "memory", havingValue = "on")
+public class MemoryAutoConfig {
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+}
