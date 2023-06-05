9a3b74802441e1d56b2df7da1eb1a5dd7733c686
Chan <33cks1423@naver.com>
2023-06-05 20:07:51 +0900

    [#71] 외부 설정 - 자바 시스템 속성

---
+++ SpringBoot/external/src/test/java/hello/external/JavaSystemProperties.java
@@ -0,0 +1,22 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+@Slf4j
+public class JavaSystemProperties {
+    public static void main(String[] args) {
+        Properties properties = System.getProperties();
+        properties.forEach((key, value)->
+                log.info("prop {}={}", key, value));
+
+        String url = System.getProperty("url");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+
+        log.info("url={}", url);
+        log.info("username={}", username);
+        log.info("password={}", password);
+    }
+}
