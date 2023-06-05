c32c695e1f91a616fca8cb0d07378c8612f651bc
Chan <33cks1423@naver.com>
2023-06-05 20:48:22 +0900

    [#71] 외부 설정 - 커맨드 라인 옵션 인수와 스프링 부트

---
+++ SpringBoot/external/src/main/java/hello/CommandLineBean.java
@@ -0,0 +1,28 @@
+package hello;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class CommandLineBean {
+
+    private final ApplicationArguments arguments;
+
+    public CommandLineBean(ApplicationArguments arguments) {
+        this.arguments = arguments;
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("source {}", List.of(arguments.getSourceArgs()));
+        log.info("optionNames {}", arguments.getOptionNames());
+        for (String optionName : arguments.getOptionNames()) {
+            log.info("option arg {}={}", optionName, arguments.getOptionValues(optionName));
+        }
+    }
+}
