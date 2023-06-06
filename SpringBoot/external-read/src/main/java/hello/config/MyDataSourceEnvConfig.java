c384f4d864f057957a817e4008bfb797150e4c73
Chan <33cks1423@naver.com>
2023-06-06 18:18:39 +0900

    [#72] 외부 설정 사용 - Environment

---
+++ SpringBoot/external-read/src/main/java/hello/config/MyDataSourceEnvConfig.java
@@ -0,0 +1,33 @@
+package hello.config;
+
+import hello.datasource.MyDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.time.Duration;
+import java.util.List;
+
+@Slf4j
+@Configuration
+public class MyDataSourceEnvConfig {
+
+    private final Environment env;
+
+    public MyDataSourceEnvConfig(Environment environment) {
+        this.env = environment;
+    }
+
+    @Bean
+    public MyDataSource myDataSource() {
+        String url = env.getProperty("my.datasource.url");
+        String password = env.getProperty("my.datasource.password");
+        String username = env.getProperty("my.datasource.username");
+        int maxConnection = env.getProperty("my.datasource.etc.max-connetion", Integer.class);
+        Duration timeout = env.getProperty("my.datasource.etc.timeout", Duration.class);
+        List<String> options = env.getProperty("my.datasource.etc.options", List.class);
+
+        return new MyDataSource(url, username, password, maxConnection, timeout, options);
+    }
+}
