b455a84ce85dae937ca1124b67f9653ef8c82372
Chan <33cks1423@naver.com>
2023-06-08 21:28:13 +0900

    [#75] 메트릭 등록5 - 게이지

--- SpringBoot/actuator/src/main/java/hello/ActuatorApplication.java
+++ SpringBoot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,5 +1,6 @@
 package hello;
 
+import hello.order.guage.StockConfigV2;
 import hello.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV4.class)
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
