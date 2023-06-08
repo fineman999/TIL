a2cc148c5dd6f9e3f132a4c1ed54332520945e2a
Chan <33cks1423@naver.com>
2023-06-08 20:58:28 +0900

    [#75] 메트릭 등록4 - @Timed

--- SpringBoot/actuator/src/main/java/hello/ActuatorApplication.java
+++ SpringBoot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,13 +1,13 @@
 package hello;
 
-import hello.order.v3.OrderConfigV3;
+import hello.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV3.class)
+@Import(OrderConfigV4.class)
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
