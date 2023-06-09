855afaccab6f8fa2cd41f996d9618ef43ac4827f
Chan <33cks1423@naver.com>
2023-06-09 21:08:01 +0900

    [#76] Display names

--- SpringTest/junit/src/test/java/hello/junit/DemoUtilsTest.java
+++ SpringTest/junit/src/test/java/hello/junit/DemoUtilsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DemoUtilsTest {
 
     DemoUtils demoUtils;
@@ -13,9 +14,28 @@ class DemoUtilsTest {
     @BeforeEach
     void setupBeforeEach() {
         demoUtils = new DemoUtils();
-        log.info("@BeforeEach executes before the execution of each test method");
     }
 
+
+    @Test
+    @DisplayName("Equals and Not Equals")
+    void testEqualsAndNotEquals() {
+
+        assertThat(6).isEqualTo(demoUtils.add(2, 4));
+        assertThat(demoUtils.add(1, 9)).isNotEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("Null and Not Null")
+    void testNullAndNotNull() {
+        String str1 = null;
+        String str2 = "test unit";
+
+        assertThat(demoUtils.checkNull(str1)).isNull();
+        assertThat(demoUtils.checkNull(str2)).isNotNull();
+
+    }
+/*
     @AfterEach
     void tearDownAfterEach() {
         log.info("Running @AfterEach");
@@ -31,29 +51,6 @@ class DemoUtilsTest {
     static void tearDownAfterAll() {
         log.info("@AfterAll 테스트 메서드를 시작한 후에 한번만 실행됩니다.");
     }
-
-
-
-
-    @Test
-    void testEqualsAndNotEquals() {
-        log.info("Running test: testEqualsAndNotEquals");
-
-
-        assertThat(6).isEqualTo(demoUtils.add(2, 4));
-        assertThat(demoUtils.add(1, 9)).isNotEqualTo(6);
-    }
-
-    @Test
-    void testNullAndNotNull() {
-        log.info("Running test: testNullAndNotNull");
-
-        String str1 = null;
-        String str2 = "test unit";
-
-        assertThat(demoUtils.checkNull(str1)).isNull();
-        assertThat(demoUtils.checkNull(str2)).isNotNull();
-
-    }
+    */
 
 }
