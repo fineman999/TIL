0686a63f22633d58f03428089ff98fa5119dd53a
Chan <33cks1423@naver.com>
2023-06-10 16:06:12 +0900

    [#77] refactoring

--- SpringTest/junit/src/main/java/hello/tdd/FizzBuzz.java
+++ SpringTest/junit/src/main/java/hello/tdd/FizzBuzz.java
@@ -1,6 +1,20 @@
 package hello.tdd;
 
 public class FizzBuzz {
+
+    public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) result.append("Fizz");
+
+        if (i % 5 == 0) result.append("Buzz");
+
+        if (result.isEmpty()) result.append(i);
+
+        return result.toString();
+    }
+
+/*
     public static String compute(int i) {
 
         if ((i % 3 == 0) && (i % 5 == 0)) {
@@ -13,4 +27,5 @@ public class FizzBuzz {
         }
         return Integer.toString(i);
     }
+    */
 }
