95fe13e0e1459a270534a557e7b3260335c29a00
Chan <33cks1423@naver.com>
2023-06-10 17:24:17 +0900

    [#78] application.properties

--- SpringTest/MvcTestingExample/src/test/java/hello/component/mvctestingexample/MvcTestingExampleApplicationTests.java
+++ SpringTest/MvcTestingExample/src/test/java/hello/component/mvctestingexample/MvcTestingExampleApplicationTests.java
@@ -1,10 +1,54 @@
 package hello.component.mvctestingexample;
 
+import hello.component.mvctestingexample.models.CollegeStudent;
+import hello.component.mvctestingexample.models.StudentGrades;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Slf4j
 @SpringBootTest
 class MvcTestingExampleApplicationTests {
+
+    private static int count = 0;
+
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @Value("${info.school.name}")
+    private String SchoolName;
+
+    @Autowired
+    CollegeStudent student;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    @BeforeEach
+    public void beforeEach() {
+        count = count + 1;
+        log.info("Testing: {} which is {}", appName, appDescription);
+        log.info("Version: {}. Execution of test method {}", appVersion, count);
+        student.setFirstname("Eric");
+        student.setLastname("Roby");
+        student.setEmailAddress("erick.naver.com");
+        studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList(100.0, 85.0, 76.5, 91.75)));
+        student.setStudentGrades(studentGrades);
+
+    }
+
 
     @Test
     void contextLoads() {
