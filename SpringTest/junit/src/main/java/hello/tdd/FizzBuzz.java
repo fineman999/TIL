7074ab43ffac88b849e54424103c44ee0fe74a9f
Chan <33cks1423@naver.com>
2023-06-10 16:02:32 +0900

    [#77] FizzBuzz Project - Write code to make the test pass - 2

--- SpringTest/junit/src/main/java/hello/tdd/FizzBuzz.java
+++ SpringTest/junit/src/main/java/hello/tdd/FizzBuzz.java
@@ -2,11 +2,15 @@ package hello.tdd;
 
 public class FizzBuzz {
     public static String compute(int i) {
+
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            return "FizzBuzz";
+        }
         if (i % 3 == 0) {
             return "Fizz";
         } else if (i % 5 == 0) {
             return "Buzz";
         }
-        return null;
+        return Integer.toString(i);
     }
 }
--- SpringTest/junit/src/test/java/hello/tdd/FizzBuzzTest.java
+++ SpringTest/junit/src/test/java/hello/tdd/FizzBuzzTest.java
@@ -1,18 +1,12 @@
 package hello.tdd;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
-
 import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.api.Fail.fail;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FizzBuzzTest {
-    // 숫자가 3의 배수이면 Fizz를 출력
-    // 숫자가 5의 배수이면 Buzz를 출력
-    // 숫자가 3과 5의 배수이면 FizzBuzz를 출력
-    // 숫자가 3과 5의 배수가 아니면 숫자 출력
 
+    // 숫자가 3의 배수이면 Fizz를 출력
     @DisplayName("Divisible by Tree")
     @Test
     @Order(1)
@@ -22,14 +16,33 @@ class FizzBuzzTest {
         assertThat(expected).isEqualTo(FizzBuzz.compute(3));
     }
 
+    // 숫자가 5의 배수이면 Buzz를 출력
     @DisplayName("Divisible by Five")
     @Test
-    @Order(1)
+    @Order(2)
     void testForDivisibleByFive() {
         String expected = "Buzz";
-
         assertThat(expected).isEqualTo(FizzBuzz.compute(5));
     }
 
+
+    // 숫자가 3과 5의 배수이면 FizzBuzz를 출력
+    @DisplayName("Divisible by Three and Five")
+    @Test
+    @Order(3)
+    void testForDivisibleByThreeAndFive() {
+        String expected = "FizzBuzz";
+        assertThat(expected).isEqualTo(FizzBuzz.compute(15));
+    }
+
+    // 숫자가 3과 5의 배수가 아니면 숫자 출력
+    @DisplayName("Not Divisible by Three and Five")
+    @Test
+    @Order(3)
+    void testForNotDivisibleByThreeAndFive() {
+        String expected = "1";
+
+        assertThat(expected).isEqualTo(FizzBuzz.compute(1));
+    }
 
 }
