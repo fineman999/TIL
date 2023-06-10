c81b8b77ca570624cd4abc61c39d7e2a029acd4f
Chan <33cks1423@naver.com>
2023-06-10 21:45:21 +0900

    [#81] TDD Step3: 성공하는 테스트 코드 작성

--- SpringTest/studentGrade/src/main/java/hello/studentgrade/repository/StudentDao.java
+++ SpringTest/studentGrade/src/main/java/hello/studentgrade/repository/StudentDao.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentDao extends CrudRepository<CollegeStudent, Integer> {
 
+    CollegeStudent findByEmailAddress(String emailAddress);
 }
--- SpringTest/studentGrade/src/test/java/hello/studentgrade/StudentGradeApplicationTests.java
+++ SpringTest/studentGrade/src/test/java/hello/studentgrade/StudentGradeApplicationTests.java
@@ -1,23 +1,31 @@
 package hello.studentgrade;
 
+import hello.studentgrade.models.CollegeStudent;
+import hello.studentgrade.repository.StudentDao;
 import hello.studentgrade.service.StudentAndGradeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class StudentGradeApplicationTests {
 
     @Autowired
     private StudentAndGradeService studentService;
+
+    @Autowired
+    private StudentDao studentDao;
+
     @Test
     void createStudentService() {
         studentService.createStudent("Chad", "Darby", "test@naver.com");
 
-//        studentDao.findByEmailAddress("test@naver.com");
-//
-//        Assertions.assertThat(student.getEmailAddress())
-//                .isEqal
+        CollegeStudent student = studentDao.findByEmailAddress("test@naver.com");
+
+        assertThat(student.getEmailAddress())
+                .isEqualTo("test@naver.com");
     }
 
 }
