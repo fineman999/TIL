e8fd233ab6591c5376cd7f0d49ff4ee69c229a9d
Chan <33cks1423@naver.com>
2023-06-11 20:09:19 +0900

    [#85] controller - Passing Test

--- SpringTest/studentGrade/src/main/java/hello/studentgrade/controller/GradebookController.java
+++ SpringTest/studentGrade/src/main/java/hello/studentgrade/controller/GradebookController.java
@@ -79,10 +79,49 @@ public class GradebookController {
 		} else {
 			m.addAttribute("scienceAverage", "N/A");
 		}
-
-
-
 		return "studentInformation";
+	}
+
+	@PostMapping("/grades")
+	public String createGrade(@RequestParam("grade") double grade,
+							  @RequestParam("gradeType") String gradeType,
+							  @RequestParam("studentId") int studentId,
+							  Model m) {
+		if (!studentService.checkIfStudentIsNull(studentId)) {
+			return "error";
 		}
+		boolean success = studentService.createGrade(grade, studentId, gradeType);
+
+		if (!success) {
+			return "error";
+		}
+
+		GradebookCollegeStudent studentEntity = studentService.studentInformation(studentId);
+		m.addAttribute("student", studentEntity);
+		if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+			m.addAttribute("mathAverage", studentEntity.getStudentGrades().findGradePointAverage(
+					studentEntity.getStudentGrades().getMathGradeResults()
+			));
+		} else {
+			m.addAttribute("mathAverage", "N/A");
+		}
+
+		if (studentEntity.getStudentGrades().getHistoryGradeResults().size() > 0) {
+			m.addAttribute("historyAverage", studentEntity.getStudentGrades().findGradePointAverage(
+					studentEntity.getStudentGrades().getHistoryGradeResults()
+			));
+		} else {
+			m.addAttribute("historyAverage", "N/A");
+		}
+
+		if (studentEntity.getStudentGrades().getScienceGradeResults().size() > 0) {
+			m.addAttribute("scienceAverage", studentEntity.getStudentGrades().findGradePointAverage(
+					studentEntity.getStudentGrades().getScienceGradeResults()
+			));
+		} else {
+			m.addAttribute("scienceAverage", "N/A");
+		}
+		return "studentInformation";
+	}
 
 }
