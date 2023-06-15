c2d14790a0ec6cf27cfadc2d09c7823ff210b1e7
Chan <33cks1423@naver.com>
2023-06-15 20:23:49 +0900

    [#88] SetUp

--- SpringTest/testDriven/src/main/java/hello/testdriven/controller/UserCreateController.java
+++ SpringTest/testDriven/src/main/java/hello/testdriven/controller/UserCreateController.java
@@ -6,6 +6,7 @@ import hello.testdriven.repository.UserEntity;
 import hello.testdriven.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Tag(name = "유저(users)")
 @RestController
 @RequestMapping("/api/users")
@@ -21,6 +23,7 @@ public class UserCreateController {
 
     private final UserController userController;
     private final UserService userService;
+
 
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody UserCreateDto userCreateDto) {
