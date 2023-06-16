49455e85b3d60006db8192ffa64da349a4e8e455
Chan <33cks1423@naver.com>
2023-06-16 17:06:31 +0900

    [#88] userCreateController 메소드에 테스트 추가

---
+++ SpringTest/testDriven/src/test/java/hello/testdriven/controller/UserCreateControllerTest.java
@@ -0,0 +1,67 @@
+package hello.testdriven.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.testdriven.model.dto.UserCreateDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+class UserCreateControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JavaMailSender mailSender;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    @DisplayName("사용자는 회원가입을 할 수 있고 회원가입된 사용자는 PENDING 상태이다.")
+    void createUser() throws Exception {
+
+        // given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("spring3@naver.com")
+                .nickname("hahaha")
+                .address("busan")
+                .build();
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        // when
+        // then
+        mockMvc.perform(
+                post("/api/users")
+                        .header("EMAIL","spring3@naver.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("spring3@naver.com"))
+                .andExpect(jsonPath("$.nickname").value("hahaha"))
+                .andExpect(jsonPath("$.status").value("PENDING"));
+    }
+}
