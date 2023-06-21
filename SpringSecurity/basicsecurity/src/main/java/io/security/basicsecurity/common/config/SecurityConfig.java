25228667fc01f11c9568ccc7868f5fef3305ffb9
Chan <33cks1423@naver.com>
2023-06-21 21:27:45 +0900

    [#93] Security Setup

---
+++ SpringSecurity/basicsecurity/src/main/java/io/security/basicsecurity/common/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package io.security.basicsecurity.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests
+                                .anyRequest().authenticated()
+                ).formLogin(withDefaults());
+        return http.build();
+    }
+}
