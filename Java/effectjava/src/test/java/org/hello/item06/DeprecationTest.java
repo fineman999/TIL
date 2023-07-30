fb6f3be103d52e25a2b4608234b2c530d964e905
Chan <33cks1423@naver.com>
2023-07-30 16:42:37 +0900

    [#107] Deprecation

---
+++ Java/effectjava/src/test/java/org/hello/item06/DeprecationTest.java
@@ -0,0 +1,15 @@
+package org.hello.item06;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeprecationTest {
+
+    @Test
+    @DisplayName("삭제될 버전을 알려준다.")
+    void name() {
+        Deprecation deprecation = new Deprecation();
+    }
+}
