29684dca022b699edfed32606c55915b30aa0737
Chan <33cks1423@naver.com>
2023-08-10 21:03:01 +0900

    [#111] 대칭성: A.equals(B) == B.equals(A)

---
+++ Java/effectjava/src/test/java/org/hello/item10/CaseInsensitiveStringTest.java
@@ -0,0 +1,25 @@
+package org.hello.item10;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CaseInsensitiveStringTest {
+
+    @Test
+    @DisplayName("대칭성 위배 테스트")
+    void equals() {
+        CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
+        String s = "polish";
+
+        assertThat(cis.equals(s)).isTrue();
+        assertThat(s.equals(cis)).isFalse();
+        List<CaseInsensitiveString> list = new ArrayList<>();
+        list.add(cis);
+        assertThat(list.contains(s)).isFalse();
+    }
+}
