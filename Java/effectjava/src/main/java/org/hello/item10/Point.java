a3125f19c7c54a3fcd2486370f814be2607a6034
Chan <33cks1423@naver.com>
2023-08-12 15:46:54 +0900

    [#111] equals 구현 방법과 주의사항

--- Java/effectjava/src/main/java/org/hello/item10/Point.java
+++ Java/effectjava/src/main/java/org/hello/item10/Point.java
@@ -12,10 +12,17 @@ public class Point {
 
     @Override
     public boolean equals(Object o) {
+        // 1. 반사성
+        if (this == o) {
+            return true;
+        }
+        // 2. 올바른 타입 확인
         if (!(o instanceof Point)) {
             return false;
         }
+        // 3. 형 변환
         Point p = (Point)o;
+        // 4. 핵심적인 필드들이 모두 일치하는지 검사
         return p.x == x && p.y == y;
     }
 }
