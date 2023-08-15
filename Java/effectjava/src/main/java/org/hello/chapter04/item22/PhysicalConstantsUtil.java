f61ce81497be7440bf438857ffb285f1e6dcb4b7
Chan <33cks1423@naver.com>
2023-08-15 22:48:35 +0900

    [#122] 상수를 정의해야할 때 인터페이스를 사용하지 말자

---
+++ Java/effectjava/src/main/java/org/hello/chapter04/item22/PhysicalConstantsUtil.java
@@ -0,0 +1,10 @@
+package org.hello.chapter04.item22;
+
+public final class PhysicalConstantsUtil {
+    private PhysicalConstantsUtil() {
+    } // 인스턴스화 방지
+
+    public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+    public static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+    public static final double ELECTRON_MASS = 9.109_383_56e-31;
+}
