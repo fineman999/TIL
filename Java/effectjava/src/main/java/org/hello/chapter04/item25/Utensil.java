4741a138f322f80972f0af098c61d703f162e517
Chan <33cks1423@naver.com>
2023-08-16 23:48:24 +0900

    [#125] 톱 레벨 클래스

---
+++ Java/effectjava/src/main/java/org/hello/chapter04/item25/Utensil.java
@@ -0,0 +1,10 @@
+package org.hello.chapter04.item25;
+
+// 코드 25-1 두 클래스가 한 파일(Utensil.java)에 정의되었다! - 따라 하지 말 것! (151쪽)
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
