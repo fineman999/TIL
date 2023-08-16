16f42b6ec94bb71cce430600a5f9a1841b487d4e
Chan <33cks1423@naver.com>
2023-08-16 23:09:04 +0900

    [#123] 클래스 계층 구조

---
+++ Java/effectjava/src/main/java/org/hello/chapter04/item23/hierarchy/Rectangle.java
@@ -0,0 +1,13 @@
+package org.hello.chapter04.item23.hierarchy;
+
+class Rectangle extends Figure {
+    final double length;
+    final double width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override double area() { return length * width; }
+}
---
+++ Java/effectjava/src/main/java/org/hello/chapter04/item23/hierarchy/Square.java
@@ -0,0 +1,7 @@
+package org.hello.chapter04.item23.hierarchy;
+
+class Square extends Rectangle {
+    Square(double side) {
+        super(side, side);
+    }
+}
