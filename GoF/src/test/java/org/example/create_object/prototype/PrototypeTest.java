730f9e11e63fb4945a87dfc5e38485b853e754d2
Chan <33cks1423@naver.com>
2023-09-24 16:45:48 +0900

    [#135] 프로토타입 패턴 - 활용

--- GoF/src/test/java/org/example/create_object/prototype/PrototypeTest.java
+++ GoF/src/test/java/org/example/create_object/prototype/PrototypeTest.java
@@ -5,6 +5,10 @@ import org.example.create_object.prototype._02_after.GitHubRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PrototypeTest {
@@ -35,4 +39,29 @@ class PrototypeTest {
         assertThat(clone.getRepository()).isSameAs(gitHubIssue.getRepository()); // clone.getRepository() == gitHubIssue.getRepository() why? 같은 객체(레퍼런스)이다.
         assertThat(clone.getUrl()).isNotSameAs(gitHubIssue.getUrl()); // clone.getUrl() != gitHubIssue.getUrl() why? 다른 객체(레퍼런스)이다.
     }
+
+    @Test
+    @DisplayName("실무에서 clone 사용하기 - 프로토타입 패턴 x")
+    void test1() {
+
+        GitHubRepository repository1 = new GitHubRepository();
+        repository1.setUser("jojoldu");
+        repository1.setName("springboot-webservice");
+
+        GitHubRepository repository2 = new GitHubRepository();
+        repository2.setUser("nodejs");
+        repository2.setName("nodejs-book");
+
+
+        List<GitHubRepository> repositories = Arrays.asList(repository1, repository2);
+
+
+        // 인터페이스를 사용하기 때문에(인터페이스는 clone이 없다.) 생성자를 통해 객체를 생성해야 한다.
+        List<GitHubRepository> clone = new ArrayList<>(repositories);
+
+        assertThat(clone).isNotSameAs(repositories); // clone != repositories why? 다른 객체(레퍼런스)이다.
+        assertThat(clone).isEqualTo(repositories); // clone.equals(repositories) == true why? 값은 같지만 다른 객체이다.
+    }
+
+
 }
