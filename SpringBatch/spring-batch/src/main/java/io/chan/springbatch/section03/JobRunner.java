2483b3f5643be0bfe2bf8b4df329c9b9eee5f61f
Chan <33cks1423@naver.com>
2023-10-21 21:03:23 +0900

    [#147] package correct

--- SpringBatch/spring-batch/src/main/java/io/chan/springbatch/section03/JobRunner.java
+++ SpringBatch/spring-batch/src/main/java/io/chan/springbatch/section03/JobRunner.java
@@ -7,7 +7,6 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
 
 //@Component
 @RequiredArgsConstructor
