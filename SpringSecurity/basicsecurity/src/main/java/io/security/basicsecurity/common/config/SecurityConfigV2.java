98ad35ee7827f5937b5d9cf5b20cda0fdcb07496
Chan <33cks1423@naver.com>
2023-11-02 21:34:02 +0900

    [#93] 스프링 시큐리티 V2: Form Login 이해

--- SpringSecurity/basicsecurity/src/main/java/io/security/basicsecurity/common/config/SecurityConfigV2.java
+++ SpringSecurity/basicsecurity/src/main/java/io/security/basicsecurity/common/config/SecurityConfigV2.java
@@ -1,5 +1,6 @@
 package io.security.basicsecurity.common.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigV2 {
@@ -16,7 +18,24 @@ public class SecurityConfigV2 {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .anyRequest().authenticated()
                 );
-        http.formLogin(Customizer.withDefaults());
+        http.formLogin(
+            formLogin -> formLogin
+                .loginPage("/loginPage")
+                .defaultSuccessUrl("/user", true)
+                .failureUrl("/loginPage?error")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler((request, response, authentication) -> {
+                    log.info("authentication = {}", authentication);
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {
+                    log.info("exception = {}", exception.getMessage());
+                    response.sendRedirect("/loginPage");
+                })
+                .permitAll()
+        );
 
         return http.build();
     }
