f58cdb760731aba4e020dfb26f2ce525addc3bba
Chan <33cks1423@naver.com>
2023-11-07 23:06:06 +0900

    [#93] 스프링 시큐리티 V2: 인증 API – 동시 세션 제어 / 세션고정보호 / 세션 정책

--- SpringSecurity/basicsecurity/src/main/java/io/security/basicsecurity/common/config/SecurityConfigV2.java
+++ SpringSecurity/basicsecurity/src/main/java/io/security/basicsecurity/common/config/SecurityConfigV2.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -63,6 +64,13 @@ public class SecurityConfigV2 {
                 .userDetailsService(userDetailsService)
         );
 
+        http.sessionManagement(
+            sessionManagement -> sessionManagement
+                    .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .sessionFixation().changeSessionId()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true)
+        );
         return http.build();
     }
 }
