e77d62ff98483c85785b9d23375940db505a6aa6
chpark <chpark@witchcompany.io>
2023-11-20 13:54:54 +0900

    [#148] WebIgnore 설정

--- SpringSecurity/spring-core-security/src/main/java/io/chan/springcoresecurity/security/config/SecurityConfig.java
+++ SpringSecurity/spring-core-security/src/main/java/io/chan/springcoresecurity/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -15,6 +16,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    // please use permitAll via HttpSecurity#authorizeHttpRequests instead.
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring()
+//                .requestMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
+//    }
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
@@ -38,10 +46,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
             auth -> auth
+                .requestMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico").permitAll()
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/mypage").hasRole("USER")
-                .requestMatchers("/messages").hasRole("MANAGER")
-                .requestMatchers("/config").hasRole("ADMIN")
+                .requestMatchers("/messages").hasAnyRole("MANAGER", "ADMIN")
+                .requestMatchers("/config").hasAnyRole("ADMIN", "MANAGER", "USER")
                 .anyRequest().authenticated()
         ).formLogin(Customizer.withDefaults());
         return http.build();
