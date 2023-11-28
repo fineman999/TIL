bfeedfc66f70e0718c1fd6afb879bad54476ca0d
chpark <chpark@witchcompany.io>
2023-11-28 17:04:22 +0900

    [#95] Cors이해

--- SpringSecurity/spring-security-oauth2/cors-2/src/main/java/io/security/cors2/SecurityConfig.java
+++ SpringSecurity/spring-security-oauth2/cors-2/src/main/java/io/security/cors2/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@Configration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
@@ -17,7 +18,6 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
         http.cors(httpSecurityCorsConfigurer -> {
-
             httpSecurityCorsConfigurer.configurationSource(new CorsConfigurationSource() {
                    @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
--- SpringSecurity/spring-security-oauth2/src/main/java/io/security/oauth2/common/config/SecurityConfig.java
+++ SpringSecurity/spring-security-oauth2/src/main/java/io/security/oauth2/common/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package io.security.oauth2.common.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 import static org.springframework.security.config.Customizer.*;
 
@@ -15,21 +22,37 @@ import static org.springframework.security.config.Customizer.*;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-//                        .anyRequest().authenticated()
-//                );
-//        http.formLogin(withDefaults());
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().authenticated()
+                );
+//      form login is enabled by default
+        http.formLogin(withDefaults());
+
+        // http basic is enabled by default
+        http.httpBasic(withDefaults());
+
+        // customAuthenticationEntryPoint is enabled by default
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+            .authenticationEntryPoint(new AuthenticationEntryPoint
+                () {
+                      @Override
+                      public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                          System.out.printf("AuthenticationEntryPoint commence");
+                      }
+                  }
+            )
+        );
+        return http.build();
+    }
     @Bean
     public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .anyRequest().authenticated()
                 );
         http.httpBasic(withDefaults());
-        http.apply(new CustomSecurityConfigurer().setFlag(false));
+//        http.apply(new CustomSecurityConfigurer().setFlag(false));
         return http.build();
     }
 }
