a3a292073c4417f1ba3aea6602a4dae5677ef3c1
chpark <chpark@witchcompany.io>
2023-11-30 12:10:29 +0900

    [#152] 프로젝트 세팅

---
+++ SpringSecurity/spring-security-oauth2-v2/src/main/java/io/chan/springsecurityoauth2v2/SpringSecurityOauth2V2Application.java
@@ -0,0 +1,13 @@
+package io.chan.springsecurityoauth2v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2V2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2V2Application.class, args);
+    }
+
+}
---
+++ SpringSecurity/spring-security-oauth2-v2/src/test/java/io/chan/springsecurityoauth2v2/SpringSecurityOauth2V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chan.springsecurityoauth2v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2V2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
