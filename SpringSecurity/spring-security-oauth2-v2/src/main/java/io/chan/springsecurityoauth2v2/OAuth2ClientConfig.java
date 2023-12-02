d9264a93eee45f9d1db6c1b8b698cde00714e407
Chan <33cks1423@naver.com>
2023-12-02 14:13:03 +0900

    [#154] OAuth 로그인 구현 - OpenID Connect 로그아웃

--- SpringSecurity/spring-security-oauth2-v2/src/main/java/io/chan/springsecurityoauth2v2/OAuth2ClientConfig.java
+++ SpringSecurity/spring-security-oauth2-v2/src/main/java/io/chan/springsecurityoauth2v2/OAuth2ClientConfig.java
@@ -1,18 +1,24 @@
 package io.chan.springsecurityoauth2v2;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ClientRegistrations;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class OAuth2ClientConfig {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,7 +32,20 @@ public class OAuth2ClientConfig {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
                 .anyRequest().permitAll()
             );
+
+        http.logout(logout -> logout
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+            );
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/loginPage");
+        return successHandler;
     }
 
 //    @Bean
