4d3dd6ad875165d1240e27121c0629674e067dd0
Chan <33cks1423@naver.com>
2023-12-02 17:06:02 +0900

    [#155] OAuth 로그인 구현 - SpringMVC 인증 객체 참조하기

--- SpringSecurity/spring-security-oauth2-v2/src/main/java/io/chan/springsecurityoauth2v2/OAuth2ClientConfig.java
+++ SpringSecurity/spring-security-oauth2-v2/src/main/java/io/chan/springsecurityoauth2v2/OAuth2ClientConfig.java
@@ -3,63 +3,23 @@ package io.chan.springsecurityoauth2v2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class OAuth2ClientConfig {
 
-    private final ClientRegistrationRepository clientRegistrationRepository;
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-////        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-////                .requestMatchers("/loginPage").permitAll()
-////                .anyRequest().authenticated()
-////            )
-////            .oauth2Login(oauth2 ->
-////                oauth2.loginPage("/loginPage")
-////            );
-////        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-////                .anyRequest().permitAll()
-////            );
-//
-//        http.logout(logout -> logout
-//                .logoutSuccessHandler(oidcLogoutSuccessHandler())
-//                .invalidateHttpSession(true)
-//                .clearAuthentication(true)
-//                .deleteCookies("JSESSIONID")
-//            );
-//        return http.build();
-//    }
-//
-//    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
-//        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-//        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
-//        return successHandler;
-//    }
-
-//    @Bean
-//    public ClientRegistrationRepository clientRegistrationRepository() {
-//        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-//    }
-//
-//    private ClientRegistration keycloakClientRegistration() {
-//        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
-//                .clientId("oauth2-client-app")
-//                .clientSecret("hXGCxVSRKC5b9Ge8H5gPWP3kjVNlP055")
-//                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-//                .registrationId("keycloak")
-//                .build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .anyRequest().authenticated()
+            );
+        http.oauth2Client(Customizer.withDefaults());
+        return http.build();
+    }
 
 }
