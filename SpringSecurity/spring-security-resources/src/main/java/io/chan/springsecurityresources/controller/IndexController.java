34102d48cacbd4c039554284124263f0aa99ea64
chpark <chpark@witchcompany.io>
2023-12-05 10:48:07 +0900

    [#96] application 설정을 통한 리소스 인증

--- SpringSecurity/spring-security-resources/src/main/java/io/chan/springsecurityresources/controller/IndexController.java
+++ SpringSecurity/spring-security-resources/src/main/java/io/chan/springsecurityresources/controller/IndexController.java
@@ -1,13 +1,19 @@
 package io.chan.springsecurityresources.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "Hello World!";
+    public Authentication index(
+            Authentication authentication
+    ) {
+        log.info("authentication: {}", authentication);
+        return authentication;
     }
 }
