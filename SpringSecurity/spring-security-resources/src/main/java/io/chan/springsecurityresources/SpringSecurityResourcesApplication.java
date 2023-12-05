ce7eebf61b5b289623158e3e223eb33d2b2db60f
chpark <chpark@witchcompany.io>
2023-12-05 09:36:29 +0900

    [#96] 프로젝트 세팅

---
+++ SpringSecurity/spring-security-resources/src/main/java/io/chan/springsecurityresources/SpringSecurityResourcesApplication.java
@@ -0,0 +1,13 @@
+package io.chan.springsecurityresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityResourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityResourcesApplication.class, args);
+    }
+
+}
---
+++ SpringSecurity/spring-security-resources/src/test/java/io/chan/springsecurityresources/SpringSecurityResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chan.springsecurityresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityResourcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
