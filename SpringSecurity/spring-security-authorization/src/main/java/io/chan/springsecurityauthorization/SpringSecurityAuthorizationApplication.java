b3c6b4bad42613657bbe02b9099b13efdd36617d
chpark <chpark@witchcompany.io>
2023-12-08 11:41:24 +0900

    [#162] OAuth 2.0 Authorization Server 소개

---
+++ SpringSecurity/spring-security-authorization/src/main/java/io/chan/springsecurityauthorization/SpringSecurityAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package io.chan.springsecurityauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthorizationApplication.class, args);
+    }
+
+}
---
+++ SpringSecurity/spring-security-authorization/src/test/java/io/chan/springsecurityauthorization/SpringSecurityAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chan.springsecurityauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
