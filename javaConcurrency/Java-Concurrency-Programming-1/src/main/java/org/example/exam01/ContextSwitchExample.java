cf2394d137b8f52337c55d82a4538c694855b3e1
chpark <chpark@witchcompany.io>
2023-12-13 16:36:00 +0900

    [#165] Context Switch

---
+++ javaConcurrency/Java-Concurrency-Programming-1/src/main/java/org/example/exam01/ContextSwitchExample.java
@@ -0,0 +1,42 @@
+package org.example.exam01;
+
+public class ContextSwitchExample {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 1: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 2: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 3: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
