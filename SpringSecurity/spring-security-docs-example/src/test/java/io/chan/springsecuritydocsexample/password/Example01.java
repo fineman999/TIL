1c1ca4b9383b259bb91879f275cef50eddf628ba
Chan <33cks1423@naver.com>
2023-12-17 17:59:43 +0900

    [#166] Crypto

--- SpringSecurity/spring-security-docs-example/src/test/java/io/chan/springsecuritydocsexample/password/Example01.java
+++ SpringSecurity/spring-security-docs-example/src/test/java/io/chan/springsecuritydocsexample/password/Example01.java
@@ -2,13 +2,30 @@ package io.chan.springsecuritydocsexample.password;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.keygen.BytesKeyGenerator;
+import org.springframework.security.crypto.keygen.KeyGenerators;
+import org.springframework.security.crypto.keygen.StringKeyGenerator;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@SpringBootTest
 public class Example01 {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     public void test() {
@@ -26,5 +43,79 @@ public class Example01 {
         Argon2PasswordEncoder encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
         String result = encoder.encode("myPassword");
         assertTrue(encoder.matches("myPassword", result));
+    }
+
+    @Test
+    public void bytesEncryptor() {
+
+        String key = KeyGenerators.string().generateKey();
+        System.out.printf("key: %s\n", key);
+
+        BytesEncryptor stronger = Encryptors.stronger("password", key);
+        byte[] encrypt = stronger.encrypt("myPassword".getBytes());
+        byte[] decrypt = stronger.decrypt(encrypt);
+        System.out.println(new String(decrypt));
+
+    }
+
+    @Test
+    public void bytesKeyGenerator() {
+        BytesKeyGenerator bytesKeyGenerator = KeyGenerators.secureRandom();
+        byte[] key = bytesKeyGenerator.generateKey();
+        System.out.println(Arrays.toString(key));
+    }
+
+    @Test
+    public void bytesKeyGenerator2() {
+        BytesKeyGenerator bytesKeyGenerator = KeyGenerators.secureRandom(16);
+        byte[] key = bytesKeyGenerator.generateKey();
+        byte[] key2 = bytesKeyGenerator.generateKey();
+        System.out.println(Arrays.toString(key));
+        System.out.println(Arrays.toString(key2));
+        Assertions.assertThat(key).isNotEqualTo(key2);
+    }
+
+    @Test
+    public void bytesKeyGeneratorShared() {
+        BytesKeyGenerator bytesKeyGenerator = KeyGenerators.shared(16);
+        byte[] key = bytesKeyGenerator.generateKey();
+        System.out.println(Arrays.toString(key));
+        byte[] key2 = bytesKeyGenerator.generateKey();
+        System.out.println(Arrays.toString(key2));
+        Assertions.assertThat(key).isEqualTo(key2);
+    }
+
+    @Test
+    public void stringKeyGenerator() {
+        StringKeyGenerator stringKeyGenerator = KeyGenerators.string();
+        String key = stringKeyGenerator.generateKey();
+        System.out.println(key);
+        String key2 = stringKeyGenerator.generateKey();
+        System.out.println(key2);
+        Assertions.assertThat(key).isNotEqualTo(key2);
+    }
+
+    @Test
+    public void bcryptPasswordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+        String encode = bCryptPasswordEncoder.encode("myPassword");
+        System.out.println(encode);
+        Assertions.assertThat(bCryptPasswordEncoder.matches("myPassword", encode)).isTrue();
+    }
+   @Test
+    public void textEncryptor() {
+        TextEncryptor password = Encryptors.text("password", "123456");
+        String encrypt = password.encrypt("myPassword");
+        String decrypt = password.decrypt(encrypt);
+        System.out.println(decrypt);
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public PasswordEncoder delegatingPasswordEncoder() {
+
+            return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        }
     }
 }
