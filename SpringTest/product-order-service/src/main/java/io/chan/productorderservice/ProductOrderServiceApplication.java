3a5c42279c08f7587474b5b4444ebc1d1527f994
chpark <chpark@witchcompany.io>
2024-01-02 16:30:43 +0900

    [#170] 프로젝트 생성

---
+++ SpringTest/product-order-service/src/main/java/io/chan/productorderservice/ProductOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package io.chan.productorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderServiceApplication.class, args);
+    }
+
+}
---
+++ SpringTest/product-order-service/src/test/java/io/chan/productorderservice/ProductOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chan.productorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
