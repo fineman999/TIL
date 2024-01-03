046b3f1f4713a0eaad69edde28fe8e21e6ede7d1
chpark <chpark@witchcompany.io>
2024-01-03 10:43:24 +0900

    [#170] API 테스트로 전환하기

--- SpringTest/product-order-service/src/main/java/io/chan/productorderservice/product/ProductService.java
+++ SpringTest/product-order-service/src/main/java/io/chan/productorderservice/product/ProductService.java
@@ -1,9 +1,14 @@
 package io.chan.productorderservice.product;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
+@RequestMapping("/products")
 class ProductService {
 
     private final ProductPort productPort;
@@ -12,9 +17,13 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    @Transactional
-    public void addProduct(final AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(
+            @RequestBody final AddProductRequest request
+    ) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
---
+++ SpringTest/product-order-service/src/test/java/io/chan/productorderservice/product/ProductApiTest.java
@@ -0,0 +1,43 @@
+package io.chan.productorderservice.product;
+
+import io.chan.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductApiTest extends ApiTest {
+
+
+    @Test
+    void 상품_등록() {
+        final AddProductRequest request = 상품등록요청_생성();
+
+        final var response = 상품등록요청(request);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private static ExtractableResponse<Response> 상품등록요청(final AddProductRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then().log().all()
+                .extract();
+    }
+
+    private static AddProductRequest 상품등록요청_생성() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
+        return request;
+    }
+
+}
