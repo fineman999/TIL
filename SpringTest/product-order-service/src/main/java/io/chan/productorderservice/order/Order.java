7363c109e622162067e6268d63d207756c88abaf
chpark <chpark@witchcompany.io>
2024-01-05 16:17:22 +0900

    [#170] JPA 적용하기

--- SpringTest/product-order-service/src/main/java/io/chan/productorderservice/order/Order.java
+++ SpringTest/product-order-service/src/main/java/io/chan/productorderservice/order/Order.java
@@ -1,26 +1,31 @@
 package io.chan.productorderservice.order;
 
 import io.chan.productorderservice.product.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
-    private final Product product;
-    private final int quantity;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    private int quantity;
 
     public Order(final Product product, final int quantity) {
         Assert.notNull(product, "상품은 필수입니다.");
         Assert.isTrue(quantity > 0, "상품 수량은 0보다 커야 합니다.");
         this.product = product;
         this.quantity = quantity;
-
-    }
-
-    public void assignId(final Long aLong) {
-        this.id = aLong;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
