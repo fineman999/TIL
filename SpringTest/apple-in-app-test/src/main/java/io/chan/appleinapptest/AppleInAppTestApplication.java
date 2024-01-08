c7c6ae1f6a971a8d93453bb30346b370c326698c
chpark <chpark@witchcompany.io>
2024-01-08 15:33:52 +0900

    apple in app set up

---
+++ SpringTest/apple-in-app-test/src/main/java/io/chan/appleinapptest/AppleInAppTestApplication.java
@@ -0,0 +1,13 @@
+package io.chan.appleinapptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleInAppTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleInAppTestApplication.class, args);
+    }
+
+}
---
+++ SpringTest/apple-in-app-test/src/test/java/io/chan/appleinapptest/AppleInAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chan.appleinapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleInAppTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
