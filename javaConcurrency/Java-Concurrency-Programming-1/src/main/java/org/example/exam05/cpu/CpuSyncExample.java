ede4439e57e257b352d72c180037dd2e72711267
chpark <chpark@witchcompany.io>
2024-01-12 12:28:24 +0900

    [#171] 동기화와 CPU 관계

---
+++ javaConcurrency/Java-Concurrency-Programming-1/src/main/java/org/example/exam05/cpu/CpuSyncExample.java
@@ -0,0 +1,39 @@
+package org.example.exam05.cpu;
+
+public class CpuSyncExample {
+
+    private static int count = 0;
+    private static final int ITERATIONS = 100000;
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < ITERATIONS; i++) {
+                synchronized (CpuSyncExample.class) {
+                    count++;
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < ITERATIONS; i++) {
+                synchronized (CpuSyncExample.class) {
+                    count++;
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //예상 실행 결과: 200000
+        //실제 실행 결과: 200000
+        System.out.printf("예상 실행 결과: %d\n", ITERATIONS * 2);
+        System.out.printf("실제 실행 결과: %d\n", count);
+    }
+}
