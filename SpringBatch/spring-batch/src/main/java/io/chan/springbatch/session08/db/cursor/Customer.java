bac87100d2e10f6907a426d181093d6bb6f47106
chan <33cks1423@naver.com>
2024-02-29 10:28:56 +0900

    [#184] DB - JpaCursorItemReader

--- SpringBatch/spring-batch/src/main/java/io/chan/springbatch/session08/db/cursor/Customer.java
+++ SpringBatch/spring-batch/src/main/java/io/chan/springbatch/session08/db/cursor/Customer.java
@@ -2,6 +2,16 @@ package io.chan.springbatch.session08.db.cursor;
 
 import java.time.LocalDateTime;
 
+/**
+ * JPA 사용시 어노테이션을 등록하자.
+ * @param id
+ * @param name
+ * @param email
+ * @param phone
+ * @param address
+ * @param createdDate
+ * @param updatedDate
+ */
 public record Customer (
         Long id,
         String name,
