b6ae24c785bca86aa42b8b16113e5a55837980f0
chan <33cks1423@naver.com>
2024-03-03 16:33:31 +0900

    [#185] Enable Cache 설정

---
+++ SpringTest/spring-ngrinder-test/src/main/java/io/chan/springngrindertest/config/CacheUtils.java
@@ -0,0 +1,11 @@
+package io.chan.springngrindertest.config;
+
+public class CacheUtils {
+    public static final String NOTICE_FIND_ALL ="Notice.findAll";
+    public static final String NOTICE_FIND_BY_PAGE = "Notice.findByPage";
+
+    private CacheUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+}
