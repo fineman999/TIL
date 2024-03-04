49b2ccc7e05857fc0b012b42a10d5070e092a444
chan <33cks1423@naver.com>
2024-03-04 20:38:57 +0900

    [#185] test: ngrinder 테스트 코드

---
+++ SpringTest/spring-ngrinder-test/src/test/java/io/chan/springngrindertest/controller/NoticeControllerTest.java
@@ -0,0 +1,42 @@
+package io.chan.springngrindertest.controller;
+
+import org.junit.jupiter.api.Test;
+
+
+class NoticeControllerTest {
+    @Test
+    public void test() {
+
+
+//        String apiUrl = "http://127.0.0.1:8080/api/notices/dates";
+//        Map<String, String> params = new HashMap<>();
+//
+//        Random random = new Random();
+//        int randomMonth = random.nextInt(12) + 1; // Generate a random month (1-12)
+//        int randomDay = random.nextInt(28) + 1; // Generate a random day (1-28)
+//
+//        String randomMonthString = String.valueOf(randomMonth);
+//        String randomDayString = String.valueOf(randomDay);
+//        if (randomMonth < 10 && randomDay < 10) {
+//            randomMonthString = "0" + randomMonth;
+//            randomDayString = "0" + randomDay;
+//        } else if (randomMonth < 10) {
+//            randomMonthString = "0" + randomMonth;
+//        } else if (randomDay < 10) {
+//            randomDayString = "0" + randomDay;
+//        }
+//        // Generate startDate with random month and day in 2020
+//        params.put("startDate", "2020"+ "-" + randomMonthString + "-" + randomDayString + " 00:00:00");
+//
+//        // Generate endDate with random month and day in 2021
+//        params.put("endDate", "2021"+ "-" + randomMonthString + "-" + randomDayString + " 00:00:00");
+//        HTTPResponse response = request.GET(apiUrl, params);
+//
+//        if (response.statusCode == 301 || response.statusCode == 302) {
+//            grinder.logger.warn("Warning. The response may not be correct. The response code was {}.", response.statusCode)
+//        } else {
+//            assertThat(response.statusCode, is(200))
+//        }
+    }
+
+}
