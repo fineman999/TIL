8c53e710ea9c693581f4676c73e24287b90d387e
chan <33cks1423@naver.com>
2024-03-14 19:29:06 +0900

    [#189] set up

---
+++ Architecture/book-service/src/test/java/io/chan/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package io.chan.bookservice;
+
+import io.chan.bookservice.setup.AcceptanceTest;
+import org.junit.jupiter.api.Test;
+
+class BookServiceApplicationTests extends AcceptanceTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Architecture/book-service/src/test/java/io/chan/bookservice/setup/Testcontainers.java
@@ -0,0 +1,35 @@
+package io.chan.bookservice.setup;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+
+
+public abstract class Testcontainers {
+    private static final String MYSQL_USERNAME = "test";
+    private static final String MYSQL_PASSWORD = "1234";
+
+    public static MySQLContainer<?> mysqlContainer = createContainer();
+
+
+    private static MySQLContainer<?> createContainer() {
+        @SuppressWarnings("resource")
+        MySQLContainer<?> container = new MySQLContainer<>("mysql:8")
+                .withDatabaseName("test")
+                .withUsername(MYSQL_USERNAME)
+                .withPassword(MYSQL_PASSWORD)
+                .withReuse(true);
+
+        container.start();
+        return container;
+    }
+
+    @DynamicPropertySource
+    private static void configureProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", () -> MYSQL_USERNAME);
+        registry.add("spring.datasource.password", () -> MYSQL_PASSWORD);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+        registry.add("spring.jpa.show-sql", () -> "true");
+    }
+}
