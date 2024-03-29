a9358de867107ad079bc0b200286a5e6064a9989
chan <33cks1423@naver.com>
2024-03-29 20:24:51 +0900

    [#189] infra 구성 - BestBook, BookRental

--- Architecture/Book-rental-service/src/main/java/io/chan/bookrentalservice/config/KafkaConfig.java
+++ Architecture/Book-rental-service/src/main/java/io/chan/bookrentalservice/config/KafkaConfig.java
@@ -1,5 +1,8 @@
 package io.chan.bookrentalservice.config;
 
+import io.chan.bookrentalservice.domain.model.event.ItemRented;
+import io.chan.bookrentalservice.domain.model.event.ItemReturned;
+import io.chan.bookrentalservice.domain.model.event.OverdueCleared;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,19 +20,32 @@ import java.util.Map;
 public class KafkaConfig {
     @Value("${custom.kafka.producers.bootstrap-servers}")
     private String bootstrapServers;
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class.getName());
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class.getName());
-        return new DefaultKafkaProducerFactory<>(config);
-    }
 
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        return new KafkaTemplate<>(producerFactory(String.class));
+    }
+
+    @Bean
+    public KafkaTemplate<String, ItemRented> itemRentedKafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory(ItemRented.class));
+    }
+
+    @Bean
+    public KafkaTemplate<String, ItemReturned> itemReturnedKafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory(ItemReturned.class));
+    }
+
+    @Bean
+    public KafkaTemplate<String, OverdueCleared> overdueClearedKafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory(OverdueCleared.class));
+    }
+
+    private <T> ProducerFactory<String, T> producerFactory(Class<T> type) {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
     }
 }
