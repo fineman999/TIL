0911c8526f323bbfc77be822bb074ec2c929774a
chan <33cks1423@naver.com>
2024-04-06 21:24:02 +0900

    [#193] Consumer 테스트

---
+++ Kafka/KafkaProj-01/consumers/src/main/java/com/example/kafka/SimpleConsumer.java
@@ -0,0 +1,50 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public class SimpleConsumer {
+  private static final Logger logger = LoggerFactory.getLogger(SimpleConsumer.class.getName());
+
+  public static void main(String[] args) {
+
+    String topicName = "simple-topic";
+    Properties props = new Properties();
+    props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+    props.setProperty(
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    props.setProperty(
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+    final KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+    kafkaConsumer.subscribe(List.of(topicName));
+
+    while (true) {
+      final ConsumerRecords<String, String> consumerRecords =
+          kafkaConsumer.poll(Duration.ofMillis(1000));
+      consumerRecords.forEach(
+          record -> {
+            logger.info(
+                "key: "
+                    + record.key()
+                    + " value: "
+                    + record.value()
+                    + " partition: "
+                    + record.partition()
+                    + " offset: "
+                    + record.offset());
+          });
+    }
+  }
+}
