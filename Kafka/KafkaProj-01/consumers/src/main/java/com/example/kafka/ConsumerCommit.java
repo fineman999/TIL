8e219e977b49a3925857974ec4337e7d2d935357
chan <33cks1423@naver.com>
2024-04-09 19:42:39 +0900

    [#193] consumer commitsync & commitasync

--- Kafka/KafkaProj-01/consumers/src/main/java/com/example/kafka/ConsumerCommit.java
+++ Kafka/KafkaProj-01/consumers/src/main/java/com/example/kafka/ConsumerCommit.java
@@ -2,10 +2,11 @@ package com.example.kafka;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -24,9 +25,115 @@ public class ConsumerCommit {
         ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     props.setProperty(
         ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-    props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "6000");
+    props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
-    pollAutoCommit(props, topicName);
+    //    pollAutoCommit(props, topicName);
+    //    pollCommitSync(props, topicName);
+    pollCommitAsync(props, topicName);
+  }
+
+  private static void pollCommitAsync(final Properties props, final String topicName) {
+    try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props)) {
+      kafkaConsumer.subscribe(List.of(topicName));
+
+      final Thread mainThread = Thread.currentThread();
+      Runtime.getRuntime()
+          .addShutdownHook(
+              new Thread() {
+                @Override
+                public void run() {
+                  logger.info("main thread starts to exit by calling wakeup");
+                  kafkaConsumer.wakeup();
+                  try {
+                    mainThread.join();
+                  } catch (InterruptedException e) {
+                    logger.error(e.getMessage());
+                  }
+                }
+              });
+
+      int loopCnt = 0;
+      while (true) {
+        final ConsumerRecords<String, String> consumerRecords =
+            kafkaConsumer.poll(Duration.ofMillis(1000));
+        logger.info(
+            "######## loopCnt: {} consumerRecords count: {}", loopCnt++, consumerRecords.count());
+        consumerRecords.forEach(
+            record -> {
+              logger.info(
+                  "key: "
+                      + record.key()
+                      + " partition: "
+                      + record.partition()
+                      + " offset: "
+                      + record.offset()
+                      + " value: "
+                      + record.value());
+            });
+        kafkaConsumer.commitAsync(new OffsetCommitCallback() {
+          @Override
+          public void onComplete(final Map<TopicPartition, OffsetAndMetadata> offsets, final Exception e) {
+            if (e != null) {
+              logger.error("Commit failed for offsets {}", offsets, e);
+            } else {
+              logger.info("Commit succeeded for offsets {}", offsets);
+            }
+          }
+        });
+      }
+    } catch (WakeupException e) {
+      logger.error("Wake up the consumer");
+    }
+  }
+
+  private static void pollCommitSync(final Properties props, final String topicName) {
+    try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props)) {
+      kafkaConsumer.subscribe(List.of(topicName));
+
+      final Thread mainThread = Thread.currentThread();
+      Runtime.getRuntime()
+          .addShutdownHook(
+              new Thread() {
+                @Override
+                public void run() {
+                  logger.info("main thread starts to exit by calling wakeup");
+                  kafkaConsumer.wakeup();
+                  try {
+                    mainThread.join();
+                  } catch (InterruptedException e) {
+                    logger.error(e.getMessage());
+                  }
+                }
+              });
+
+      int loopCnt = 0;
+      while (true) {
+        final ConsumerRecords<String, String> consumerRecords =
+            kafkaConsumer.poll(Duration.ofMillis(1000));
+        logger.info(
+            "######## loopCnt: {} consumerRecords count: {}", loopCnt++, consumerRecords.count());
+        consumerRecords.forEach(
+            record -> {
+              logger.info(
+                  "key: "
+                      + record.key()
+                      + " partition: "
+                      + record.partition()
+                      + " offset: "
+                      + record.offset()
+                      + " value: "
+                      + record.value());
+            });
+        try {
+          if (consumerRecords.count() > 0) kafkaConsumer.commitSync();
+          logger.info("commitSync is called");
+        } catch (CommitFailedException e) {
+          logger.error("Commit failed");
+        }
+      }
+    } catch (WakeupException e) {
+      logger.error("Wake up the consumer");
+    }
   }
 
   private static void pollAutoCommit(Properties props, String topicName) {
@@ -35,48 +142,41 @@ public class ConsumerCommit {
 
       final Thread mainThread = Thread.currentThread();
       Runtime.getRuntime()
-              .addShutdownHook(
-                      new Thread() {
-                        @Override
-                        public void run() {
-                          logger.info("main thread starts to exit by calling wakeup");
-                          kafkaConsumer.wakeup();
-                          try {
-                            mainThread.join();
-                          } catch (InterruptedException e) {
-                            logger.error(e.getMessage());
-                          }
-                        }
-                      });
+          .addShutdownHook(
+              new Thread() {
+                @Override
+                public void run() {
+                  logger.info("main thread starts to exit by calling wakeup");
+                  kafkaConsumer.wakeup();
+                  try {
+                    mainThread.join();
+                  } catch (InterruptedException e) {
+                    logger.error(e.getMessage());
+                  }
+                }
+              });
 
       int loopCnt = 0;
       while (true) {
         final ConsumerRecords<String, String> consumerRecords =
-                kafkaConsumer.poll(Duration.ofMillis(1000));
+            kafkaConsumer.poll(Duration.ofMillis(1000));
         logger.info(
-                "######## loopCnt: {} consumerRecords count: {}", loopCnt++, consumerRecords.count());
+            "######## loopCnt: {} consumerRecords count: {}", loopCnt++, consumerRecords.count());
         consumerRecords.forEach(
-                record -> {
-                  logger.info(
-                          "key: "
-                                  + record.key()
-                                  + " partition: "
-                                  + record.partition()
-                                  + " offset: "
-                                  + record.offset()
-                                  + " value: "
-                                  + record.value());
-                });
-        try {
-          logger.info("main thread is sleeping {} ms during while loop", 10000);
-          Thread.sleep(10000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
+            record -> {
+              logger.info(
+                  "key: "
+                      + record.key()
+                      + " partition: "
+                      + record.partition()
+                      + " offset: "
+                      + record.offset()
+                      + " value: "
+                      + record.value());
+            });
       }
     } catch (WakeupException e) {
       logger.error("Wake up the consumer");
     }
   }
-
 }
