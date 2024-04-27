96695543f6b3ccc13e3067cca9e074de44b69617
chan <33cks1423@naver.com>
2024-04-27 20:56:50 +0900

    [#198] Resilience4j를 이용한 retry

--- Resilience4j/resilience4j-practice/src/main/java/io/chan/resilience4jpractice/a_retry/RetryService.java
+++ Resilience4j/resilience4j-practice/src/main/java/io/chan/resilience4jpractice/a_retry/RetryService.java
@@ -1,6 +1,8 @@
 package io.chan.resilience4jpractice.a_retry;
 
+import io.chan.resilience4jpractice.exception.IgnoreException;
 import io.chan.resilience4jpractice.exception.RetryException;
+import io.chan.resilience4jpractice.exception.SomeException;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,10 +25,16 @@ public class RetryService {
     }
 
     private String callAnotherServer(String param) {
-        // retry exception은 retry된다.
-        throw new RetryException("retry exception");
-        // ignore exception은 retry하지 않고 바로 예외가 클라이언트에게 전달된다.
-//        throw new IgnoreException("ignore exception");
+        if (param.equals("retry")) {
+            // retry exception은 retry된다.
+            throw new RetryException("retry exception");
+        } else if (param.equals("ignore")) {
+            // ignore exception은 retry하지 않고 바로 예외가 클라이언트에게 전달된다.
+            throw new IgnoreException("ignore exception");
+        } else if (param.equals("exception")) {
+      throw new SomeException("exception");
+        }
+        return "success";
     }
 
 };
