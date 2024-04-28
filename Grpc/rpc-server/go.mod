5c88466417d62ebf585615b7f3e7c6aaa2e5ca9e
chan <33cks1423@naver.com>
2024-04-28 19:44:15 +0900

    [#199] Flag를 통해 환경변수 관리하기

--- Grpc/rpc-server/go.mod
+++ Grpc/rpc-server/go.mod
@@ -1,3 +1,8 @@
 module rpc-server
 
 go 1.20
+
+require (
+	github.com/naoina/go-stringutil v0.1.0 // indirect
+	github.com/naoina/toml v0.1.1 // indirect
+)
--- Grpc/rpc-server/main.go
+++ Grpc/rpc-server/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"rpc-server/config"
+)
+
+// flag 패키지는 Go 언어에서 커맨드라인 인자를 처리하는데 사용
+// 1. 플래그의 이름("config")
+// 2. 플래그의 기본값("./config.toml")
+// 3. 플래그에 대한 설명("config path")
+var configFlag = flag.String("config", "./config.toml", "config path")
 
 func main() {
-
-	fmt.Println("hihi")
+	flag.Parse()
+	config.NewClient(*configFlag)
 }
