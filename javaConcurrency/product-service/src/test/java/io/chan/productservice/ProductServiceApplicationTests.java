c0efa47c52f833824505ea195183d4f8169cf50c
chan <33cks1423@naver.com>
2024-05-01 22:50:15 +0900

    [#200] 프로젝트 셋업

---
+++ javaConcurrency/product-service/src/test/java/io/chan/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package io.chan.productservice;
+
+import io.chan.productservice.setup.AcceptanceTest;
+import org.junit.jupiter.api.Test;
+
+
+class ProductServiceApplicationTests extends AcceptanceTest {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ javaConcurrency/product-service/src/test/java/io/chan/productservice/TestProductServiceApplication.java
@@ -0,0 +1,24 @@
+package io.chan.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestProductServiceApplication {
+
+  @Bean
+  @ServiceConnection
+  MySQLContainer<?> mysqlContainer() {
+    return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.from(ProductServiceApplication::main)
+        .with(TestProductServiceApplication.class)
+        .run(args);
+  }
+}
