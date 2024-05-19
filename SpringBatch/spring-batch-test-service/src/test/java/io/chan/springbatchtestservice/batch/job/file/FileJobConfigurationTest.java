5e0ba036c15d53a9c09acee4e64fc69822f308e9
chan <33cks1423@naver.com>
2024-05-19 18:39:44 +0900

    [#190] fileStep Test 코드 작성

--- SpringBatch/spring-batch-test-service/src/test/java/io/chan/springbatchtestservice/batch/job/file/FileJobConfigurationTest.java
+++ SpringBatch/spring-batch-test-service/src/test/java/io/chan/springbatchtestservice/batch/job/file/FileJobConfigurationTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.chan.springbatchtestservice.TestBatchConfig;
 import io.chan.springbatchtestservice.setup.AcceptanceTest;
 import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.*;
 import org.springframework.batch.test.JobLauncherTestUtils;
@@ -15,12 +18,13 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringBatchTest
-@SpringJUnitConfig(classes = {FileJobConfiguration.class, TestBatchConfig.class}) //
-public class FileJobConfigurationTest extends AcceptanceTest{
+@SpringJUnitConfig(classes = {FileJobConfiguration.class, TestBatchConfig.class})
+public class FileJobConfigurationTest extends AcceptanceTest {
 
   @Autowired private JobLauncherTestUtils jobLauncherTestUtils;
   @Autowired private JdbcTemplate jdbcTemplate;
 
+  @DisplayName("파일을 읽어서 DB에 저장하는 Job - End-To-End 테스트")
   @Test
   public void fileJobTest(@Autowired @Qualifier("fileJob") Job fileJob) throws Exception {
     // given
@@ -42,4 +46,32 @@ public class FileJobConfigurationTest extends AcceptanceTest{
         .isEqualTo(30);
   }
 
+  @DisplayName("파일을 읽어서 DB에 저장하는 Job - Step 테스트")
+  @Test
+  public void fileStepTest(
+      @Autowired @Qualifier("fileJob") Job fileJob, @Autowired @Qualifier("fileStep") Step fileStep)
+      throws Exception {
+    jobLauncherTestUtils.setJob(fileJob);
+    // given
+    JobParameters jobParameters =
+        new JobParametersBuilder()
+            .addString("requestDate", "20210101")
+            .addLong("date", new Date().getTime())
+            .toJobParameters();
+    // when
+    final JobExecution jobExecution =
+        jobLauncherTestUtils.launchStep(fileStep.getName(), jobParameters);
+
+    // then
+    assertThat(jobExecution.getExitStatus().getExitCode())
+        .isEqualTo(ExitStatus.COMPLETED.getExitCode());
+
+    final StepExecution stepExecution = jobExecution.getStepExecutions().stream().toList().get(0);
+    Assertions.assertAll(
+        () -> assertThat(stepExecution.getReadCount()).isEqualTo(30),
+        () -> assertThat(stepExecution.getWriteCount()).isEqualTo(30),
+        () -> assertThat(stepExecution.getFilterCount()).isEqualTo(0),
+        () -> assertThat(stepExecution.getCommitCount()).isEqualTo(4));
+    // CommitCount가 4인 이유는 3번째 Commit 후 다시 ChunkSize만큼 읽어야 하기 때문에 4번
+  }
 }
