23beeddc5d7768d5154ad06ef67de0940edda30a
chan <33cks1423@naver.com>
2024-05-30 10:05:32 +0900

    [#202] 제미니 매개변수 테스트

--- ai/gemini-ai-service/ai/gemini.go
+++ ai/gemini-ai-service/ai/gemini.go
@@ -26,8 +26,37 @@ func NewGemini(cfg *config.Config) (*Gemini, error) {
 		return nil, err
 	}
 
+	//var maxOutputTokens int32 = 100
+
+	/**
+	자세한 매개변수 요약표는 https://wikidocs.net/229820 에서...
+	temperature=0이나 top_p=0으로 설정했다고 해서 매번 완전히 동일한 결과만 생성하는 것은 아닙니다.
+	거대언어모델은 클라우드 환경에서 여러 노드의 리소스를 병렬적으로 사용하는 방식으로 수많은 연산을 수행할 뿐만 아니라,
+	확률분포에 동일한 확률값을 갖는 단어들도 나타날 수 있으므로 항상 같은 결과를 보장하기는 어렵습니다.
+	*/
+	/**
+	temperature를 높게 설정하면 모델이 생성하는 언어의 예측 가능성은 떨어지고 그만큼 독창성은 올라갑니다.
+	반대로 temperature가 낮아지면 안정적이면서도 일관된 답변을 생성합니다.
+	이런 이유로 temperature를 1로 설정하면 아무런 변화를 주지 않지만,
+	1보다 작은 값을 주면 뾰족한 확률분포를,
+	1보다 큰 값을 주면 펑퍼짐한 확률분포를 만들게 됩니다.
+	*/
+	//var temperature float32 = 0.5 // 확률분포를 조정하는 매개변수
+	//var topP float32 = 0.9 // top_p는 확률분포 내에서 선택할 단어의 범위를 결정하는 매개변수
+	//generationConfig := genai.GenerationConfig{
+	//StopSequences: []string{".", "!", "?"}, // 해당 문자열을 만나면 생성을 중단합니다. 최대 5개까지 설정 가능
+	//MaxOutputTokens: &maxOutputTokens, // 생성할 토큰의 최대 개수를 설정합니다. 최대 2048까지 설정 가능
+	//Temperature: &temperature, // 생성할 텍스트의 다양성을 조절합니다. 0.0에서 2.0 사이의 값을 설정합니다. 기본값은 1입니다.
+	//TopP: 0.9, // Top-p sampling을 사용하여 생성할 텍스트의 다양성을 조절합니다. 0.0에서 1.0 사이의 값을 설정합니다. 기본값은 1입니다.
+	//}
 	// The Gemini 1.5 models are versatile and work with most use cases
 	model := client.GenerativeModel("gemini-1.5-flash")
+	// 설정하기
+	//model.GenerationConfig.StopSequences = generationConfig.StopSequences
+	//model.GenerationConfig.MaxOutputTokens = generationConfig.MaxOutputTokens
+	//model.GenerationConfig.Temperature = generationConfig.Temperature
+	//model.GenerationConfig.TopP = generationConfig.TopP
+
 	return &Gemini{
 		geminiModel: model,
 		client:      client,
@@ -41,6 +70,12 @@ func (g *Gemini) GenerateText(ctx context.Context, prompt string) (*genai.Genera
 	if err != nil {
 		return nil, err
 	}
+	part := resp.Candidates[0].Content.Parts[0]
+	tokens, err := g.geminiModel.CountTokens(ctx, part)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Token의 개수: ", tokens.TotalTokens)
 	return resp, nil
 }
 
