5b86186ab639724c6e3bcf79d79c0249d7f078ee
chan <33cks1423@naver.com>
2024-06-10 11:30:00 +0900

    [#199] 파이썬 graceshutdown 적용

--- Grpc/grpc-tutorial-python/app/main.py
+++ Grpc/grpc-tutorial-python/app/main.py
@@ -1,7 +1,7 @@
 # app/main.py
 
 from concurrent import futures
-import grpc
+import grpc, signal
 from app.proto import myapp_pb2_grpc, myapp_pb2
 from app.services import MyAppService
 from app.config import settings
@@ -24,8 +24,26 @@ def create_server():
     return server
 
 
-def start_server():
+def serve():
     server = create_server()
+
+    def handle_sigterm(*args):
+        print("Received shutdown signal")
+        # Graceful shutdown
+        all_rpcs_done_event = server.stop(5)  # 5 seconds grace period
+        all_rpcs_done_event.wait(5)  # Wait for the grace period to complete
+        print("Server shut down gracefully")
+
+    signal.signal(signal.SIGTERM, handle_sigterm)
+    signal.signal(signal.SIGINT, handle_sigterm)
+
     server.start()
-    print("gRPC server is running on port 50051")
-    server.wait_for_termination()
+    print(f"gRPC server is running on port {settings.GRPC_PORT}")
+    try:
+        server.wait_for_termination()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt received, shutting down server")
+
+
+def start_server():
+    serve()
