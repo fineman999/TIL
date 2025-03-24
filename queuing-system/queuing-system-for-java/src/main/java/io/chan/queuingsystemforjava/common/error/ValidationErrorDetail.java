1983d071e9e7b8eafcc3d41f10fdf69d4ab4e305
chan <33cks1423@naver.com>
2025-03-24 16:21:59 +0900

    [#211] record로 변경

--- queuing-system/queuing-system-for-java/src/main/java/io/chan/queuingsystemforjava/common/error/ValidationErrorDetail.java
+++ queuing-system/queuing-system-for-java/src/main/java/io/chan/queuingsystemforjava/common/error/ValidationErrorDetail.java
@@ -1,16 +1,11 @@
 package io.chan.queuingsystemforjava.common.error;
 
-import lombok.Data;
 import org.springframework.validation.BindingResult;
 
 import java.util.List;
 
-@Data
-public class ValidationErrorDetail {
-    private final String field;
-    private final String value;
-    private final String reason;
 
+public record ValidationErrorDetail(String field, String value, String reason) {
     public static List<ValidationErrorDetail> of(final BindingResult bindingResult) {
         return bindingResult.getFieldErrors().stream()
                 .map(
